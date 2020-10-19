946bcad8d8827ea5e2478203fda6515ae8879799
Milan Mehra <keiki08@outlook.com>
2020-10-19 18:03:19 -0500

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  info: {
+    name: "invite",
+    description: "To change the server song queue volume",
+    usage: "[invite]",
+    aliases: ["inv"],
+  },
+
+  run: async function (client, message, args) {
+    let invite = new MessageEmbed()
+    .setTitle(`Invite ${client.user.username}`)
+    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+    .setColor("BLUE")
+    return message.channel.send(invite);
+  },
+};
