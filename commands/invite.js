473ded7b437abf26f9adf23192ac757b0312ac38
Milan Mehra <keiki08@outlook.com>
2020-10-20 06:47:46 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,10 +9,14 @@ module.exports = {
   },
 
   run: async function (client, message, args) {
+    
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 37080128;
+    
     let invite = new MessageEmbed()
     .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=37080128&scope=bot`)
+    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
+    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
     .setColor("BLUE")
     return message.channel.send(invite);
   },
