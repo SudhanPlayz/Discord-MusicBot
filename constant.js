8f94401506209ed9f5cab6dd51308f9ffb37cf46
Gaurav Sharma <dannasprings929@gmail.com>
2020-10-31 11:24:22 -0500

    E

---
+++ constant.js
@@ -0,0 +1,10 @@
+ws: {
+  large_threshold: 250,
+  compress: false,
+  properties: {
+    $os: browser ? 'browser' : process.platform,
+    $browser: 'discord.js'
+    $device: 'discord.js'
+  },
+    version: 6,
+},
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const constant = require('discord.js/src/util/Constants.js')
+constant.DefaultOptions.ws.properties.$browser = `Discord iOS`
+
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity("Music", {
