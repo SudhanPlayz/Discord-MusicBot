87f71dc676af44b6b02a0db6eb7c66b53e9a0165
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2020-11-25 13:32:20 +0200

    Fix bug
    Fix Cannot read property 'end' of null

--- commands/skip.js
+++ commands/skip.js
@@ -13,7 +13,16 @@ module.exports = {
     if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue)return sendError("There is nothing playing that I could skip for you.", message.channel);
-    serverQueue.connection.dispatcher.end("Skiped the music");
+    if(!serverQueue.connection)return
+if(!serverQueue.connection.dispatcher)return
+
+       try{
+      serverQueue.connection.dispatcher.end()
+      } catch (error) {
+        serverQueue.voiceChannel.leave()
+        message.client.queue.delete(message.guild.id);
+        return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+      }
     message.react("✅")
   },
 };
