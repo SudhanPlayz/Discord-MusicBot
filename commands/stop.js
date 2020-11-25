2aefadaf5d97fd47dfcbffb659c8eda00b4729ca
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2020-11-25 13:33:51 +0200

    Fix bug
    Fix Cannot read property 'end' of null

--- commands/stop.js
+++ commands/stop.js
@@ -14,8 +14,18 @@ module.exports = {
     if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue)return sendError("There is nothing playing that I could stop for you.", message.channel);
+   if(!serverQueue.connection)return
+if(!serverQueue.connection.dispatcher)return
+     try{
+      serverQueue.connection.dispatcher.end();
+      } catch (error) {
+            serverQueue.voiceChannel.leave()
+        serverQueue.songs = [];
+        message.client.queue.delete(message.guild.id);
+        return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+      }
     serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end("Stop the music");
+   serverQueue.voiceChannel.leave()
     message.react("✅")
   },
 };
