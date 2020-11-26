e09aa14d35d38c96720cf7dc6636f2f5b2d89f74
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2020-11-26 07:51:15 +0200

    fix bugs

--- commands/stop.js
+++ commands/stop.js
@@ -19,13 +19,12 @@ if(!serverQueue.connection.dispatcher)return
      try{
       serverQueue.connection.dispatcher.end();
       } catch (error) {
-            serverQueue.voiceChannel.leave()
-        serverQueue.songs = [];
+        message.guild.me.voice.channel.leave();
         message.client.queue.delete(message.guild.id);
         return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
       }
+    message.client.queue.delete(message.guild.id);
     serverQueue.songs = [];
-   serverQueue.voiceChannel.leave()
     message.react("✅")
   },
 };
