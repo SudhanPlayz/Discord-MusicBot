dd94ba997e6151ca9de073439e86fc62c35227cc
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2021-01-31 16:51:26 +0200

    Updated leave command

--- commands/leave.js
+++ commands/leave.js
@@ -10,25 +10,24 @@ module.exports = {
     },
 
     run: async function (client, message, args) {
-        const Channel = message.member.voice.channel;
-
-        if (!Channel) return sendError("Please Join A Voice Channel!", message.channel);
-
-        if (!message.guild.me.voice) return sendError("I Am Not In Any Voice Channel!", message.channel);
+        let channel = message.member.voice.channel;
+        if (!channel) return sendError("I'm sorry but you need to be in a voice channel!", message.channel);
+        if (!message.guild.me.voice.channel) return sendError("I Am Not In Any Voice Channel!", message.channel);
 
         try {
             await message.guild.me.voice.channel.leave();
         } catch (error) {
-            await message.guild.me.voice.channel.leave();
+            await message.guild.me.voice.kick(message.guild.me.id);
             return sendError("Trying To Leave The Voice Channel...", message.channel);
         }
 
         const Embed = new MessageEmbed()
-            .setAuthor("Success Leave Voice Channel", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+            .setAuthor("Leave Voice Channel", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
             .setColor("GREEN")
-            .setDescription("Left The Voice Channel.")
+            .setTitle("Success")
+            .setDescription("🎶 Left The Voice Channel.")
             .setTimestamp();
 
-        return message.channel.send(Embed).catch(() => message.channel.send("Left The Voice Channel"));
+        return message.channel.send(Embed).catch(() => message.channel.send("🎶 Left The Voice Channel :C"));
     },
 };
