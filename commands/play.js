74503f05d61a2d8814f3356536ce466612b12afb
rohan-003 <76988068+rohan-003@users.noreply.github.com>
2021-01-31 11:41:24 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -104,8 +104,8 @@ module.exports = {
     var online = afk[message.guild.id]
     if (!song){
       if (!online.afk) {
-        sendError("Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel run `!afk`\n\nThank you for using my code! [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot)", message.channel)
-        message.guild.me.voice.channel.leave();//If you want your bot stay in vc 24/7 remove this line :D
+        #sendError("Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel run `!afk`\n\nThank you for using my code! [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot)", message.channel)
+        #message.guild.me.voice.channel.leave();//If you want your bot stay in vc 24/7 remove this line :D
         message.client.queue.delete(message.guild.id);
       }
             return message.client.queue.delete(message.guild.id);
