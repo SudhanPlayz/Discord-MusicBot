1b5e76b2cb3a290707a6d4872684fe7b45c1525d
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2021-02-04 12:01:32 +0200

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -124,7 +124,7 @@ module.exports = {
                     "Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel go to `commands/play.js` and remove the line number 61\n\nThank you for using my code! [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot)",
                     message.channel
                 );
-                serverQueue.voiceChannel.leave(); //If you want your bot stay in vc 24/7 remove this line :D
+                message.guild.me.voice.channel.leave(); //If you want your bot stay in vc 24/7 remove this line :D
                 message.client.queue.delete(message.guild.id);
                 return;
             }
