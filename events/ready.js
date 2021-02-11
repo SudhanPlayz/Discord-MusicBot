e67cadb249cff8b4b8ef14397ba860d3037374a2
AmanGamer <59835188+AmanGamer@users.noreply.github.com>
2021-02-11 23:00:51 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("!play and !help", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
