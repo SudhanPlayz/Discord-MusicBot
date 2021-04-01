9a48e43a875d0cb3c196b13a149b708124545851
ColonelOU <67454598+Colonel-Ltd@users.noreply.github.com>
2021-04-01 20:46:16 +0200

    fixed bugged

--- commands/playlist.js
+++ commands/playlist.js
@@ -146,7 +146,7 @@ module.exports = {
                         if (er) {
                             if (serverQueue) {
                                 serverQueue.songs.shift();
-                                play(serverQueue.songs[0]);
+                                play(guild, serverQueue.songs[0]);
                                 return sendError(`An unexpected error has occurred.\nPossible type \`${er}\``, message.channel);
                             }
                         }
@@ -156,7 +156,7 @@ module.exports = {
                 if (serverQueue) {
                     console.log(error);
                     serverQueue.songs.shift();
-                    play(serverQueue.songs[0]);
+                    play(guild, serverQueue.songs[0]);
                 }
             }
             serverQueue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
