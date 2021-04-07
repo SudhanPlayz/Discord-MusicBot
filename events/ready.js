0b00796fa3fc49e69f8d18fc7558a85336f0b022
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-06 19:15:24 -0500

    Really simple changing status
    This is REALLY simple And 60000 Isn't api spam :3

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,14 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
+let statuses = [
+            `This Is status 1`,
+            `This Is status 2`,
+            `This Is status 3`
+        ]
+        setInterval(() => {
+            let status = statuses[Math.floor(Math.random() * statuses.length)]
+            client.user.setActivity(status, {
+                type: 'LISTENING', //can be LISTENING, WATCHING, PLAYING, STREAMING
+            });
+        }, 60000) // I really don't recommend Lowering this It could get you in trouble
+    }
