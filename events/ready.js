a816245ce26df6c0dea57fa9a8d7b5089530a444
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-06 19:19:16 -0500

    changed it so ppl don't need to edit files

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
 let statuses = [
-            `This Is status 1`,
-            `This Is status 2`,
-            `This Is status 3`
+            `Music`,
+            `Made By SudhanPlayz`,
+            `Music Is cool`
         ]
         setInterval(() => {
             let status = statuses[Math.floor(Math.random() * statuses.length)]
