32cf4176141059c1df78bde2b8df9c8c06af452c
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2021-05-03 12:10:02 +0530

    Latest commits to fix stuff (#169)
    * 24/7 Stay in vc stuff
    
    * edits and fixes (#168)
    
    Co-authored-by: Joseph <71621973+joeyk710@users.noreply.github.com>

--- commands/invite.js
+++ commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
     member: [],
   },
-  aliases: [],
+  aliases: ["inv"],
   /**
    *
    * @param {import("../structures/DiscordMusicBot")} client
@@ -26,5 +26,25 @@ module.exports = {
       .setDescription(
         `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=${client.config.ClientID}&permissions=${client.config.Permissions}&scope=bot%20${client.config.Scopes.join("%20")}&redirect_uri=${client.config.Website}${client.config.CallbackURL}&response_type=code)`);
     message.channel.send(embed);
+  },
+  SlashCommand: {
+    /**
+   *
+   * @param {import("../structures/DiscordMusicBot")} client
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+  run: async (client, interaction, args, { GuildDB }) => {
+    let embed = new MessageEmbed()
+      .setAuthor(
+        "Invite " + client.user.tag + " to your server!",
+        client.user.displayAvatarURL()
+      )
+      .setColor("BLUE")
+      .setDescription(
+        `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=${client.config.ClientID}&permissions=${client.config.Permissions}&scope=bot%20${client.config.Scopes.join("%20")}&redirect_uri=${client.config.Website}${client.config.CallbackURL}&response_type=code)`);
+    interaction.send(embed);
+  },
   },
 };
