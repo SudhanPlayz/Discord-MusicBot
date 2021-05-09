b40874f9d330dcd8e5abd9380ae886e2b6480775
sullt4 <74088167+sullt4@users.noreply.github.com>
2021-05-09 03:04:54 -0900

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
   DefaultPrefix: ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
-  Token: "" || process.env.Token, //Discord Bot Token
-  ClientID: "", //Discord Client ID
-  ClientSecret: "", //Discord Client Secret
+  Token: "ODI3ODUwMTUwNzg0OTI1Njk3.YGhBiA.xTPJ7tgRLLVBkr9KKL-EmURGuFA" || process.env.Token, //Discord Bot Token
+  ClientID: "827850150784925697", //Discord Client ID
+  ClientSecret: "gptiUr0d7FkhJ9eu0HqHZtkpIrTbtW3q", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
@@ -21,11 +21,5 @@ module.exports = {
     host: "lavalink.sudhanplayz.live",
     port: 1234,
     pass: "CodingWithSudhan",
-  },
-
-  //Please go to https://developer.spotify.com/dashboard/
-  Spotify: {
-    ClientID: "", //Spotify Client ID
-    ClientSecret: "", //Spotify Client Secret
   },
 };
