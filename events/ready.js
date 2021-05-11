d984fd0ab35ce4682a6169a06c4a5df4bdd9ed25
Joseph <71621973+joeyk710@users.noreply.github.com>
2021-05-10 23:50:37 -0400

    updated status changing
    it should work now. i tested this so many times

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,14 @@
 module.exports = async (client) => {
   client.Ready = true, 
-    client.user.setPresence({
-        activity: { 
-            name: 'Music', // The message shown
-            type: 'LISTENING', // PLAYING, WATCHING, LISTENING, STREAMING
-        },
-        status: 'online' // You can show online, idle, dnd(Do Not Disturb)
-    })
+  client.user.setPresence({
+    status: "online",  // You can show online, idle, and dnd
+    activity: {
+        name: "Music",  // The message shown
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+    }
+});
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
 client.RegisterSlashCommands();
 };
+
