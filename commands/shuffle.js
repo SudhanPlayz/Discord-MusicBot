df4a9261d7ee77b5a3493def4965ef6bc0c3ba69
Joseph <71621973+joeyk710@users.noreply.github.com>
2021-05-18 03:11:22 -0400

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -21,7 +21,7 @@ module.exports = {
         if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
         if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to use this command!**");
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **You must be in the same voice channel as me to use this command!**");
-        if (!player.queue || !player.queue.length || player.queue.length === 0) return client.sendTime(interaction, "❌ | **Not enough songs in the queue to shuffle!**");
+        if (!player.queue || !player.queue.length || player.queue.length === 0) return client.sendTime(message.channel, "❌ | **Not enough songs in the queue to shuffle!**");
         player.queue.shuffle();
         await client.sendTime(message.channel, "✅ | Shuffled the queue!");
     },
