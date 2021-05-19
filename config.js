fc41262cd2435639acc4a0db31fab6959f980cf1
Joseph <71621973+joeyk710@users.noreply.github.com>
2021-05-18 20:36:44 -0400

    accidental edit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "", //Support Server Link
+  SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
   Token: process.env.Token || "", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
@@ -10,17 +10,19 @@ module.exports = {
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Pikachu is cute", //A Secret like a password
-  IconURL: "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
-  Permissions: 2205280512, //Bot Inviting Permissions
-  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if using Heroku
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
-  //Lavalink
+  //Lavalink - Already there is a serer to connect :)
   Lavalink: {
-    id: process.env.Lavalink_ID || "Main",
-    host: process.env.Lavalink_Host || "lava.link", //website/ip of the host
-    port: process.env.Lavalink_Port || 80, //port is 80 cuz of my tutorial, edit if u have ;-;
-    pass: process.env.Lavalink_Password ||"youshallnotpass", //default pass unless you edited
+    id: "Main",
+    host: "lavalink.sudhanplayz.live",
+    port: 1234,
+    pass: "CodingWithSudhan",
   },
+
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
     ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
