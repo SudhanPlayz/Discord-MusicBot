28d709fb8b887293c4d052853cc329b67675d752
AnthonyManninh <87104782+AnthonyManninh@users.noreply.github.com>
2021-07-07 21:58:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "ODYyNTA3Mzg3OTQ0NTY2ODM2.YOZWmQ.cQyOtCKfQl9C7JNHd_C_MuneuDE", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "862507387944566836", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "_GB5F_VNL4HNkfwG8W8plEvh1YoFeiQa", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Pikachu is EPIC", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "62ccad45660943e8bf81e18e92f033b4", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "628d9b5a30374c8b885d6b8ffce2407a", //Spotify Client Secret
   },
 };
