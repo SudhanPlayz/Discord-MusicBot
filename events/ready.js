c3b021c8ecbe3d52fefc5c1e975acbdda2a32bd9
Kakarot <79494585+Kakarot-0690@users.noreply.github.com>
2021-09-06 15:18:08 +0530

    Custom Changing Status- Status Changes In Fixed Intervals
    By Default, custom_status is set as false, but if u want changing status, make it true. The Status Of The Bot Will Change Automatically In Fixed Intervals Of Time!!!
    You Can Add or remove elements from the array. You Can change the name and type for any activity and you can change the interval from line 29.
    Many people were requesting this so here you go!

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,35 @@
-module.exports = async (client) => {
-  client.Ready = true, 
+module.exports = async (client,message,args) => {
+  
+  const custom_status = "false"; //make it true If you want Custom status
+  
+  if(custom_status==="true"){//you can add or remove activities from the array as per your wish
+  const array = [
+    {
+      name: ">help",
+      type: "LISTENING"
+    },
+    {
+      name:  "Music For "+client.users.cache.size+" users" ,//client.users.cache.size extracts number of bot users ;-;
+      type: "PLAYING"
+    },
+  
+    { 
+      name: ""+client.guilds.cache.size+" Servers!",//client.guilds.cache.size extracts number of guilds the bot is in
+      type: "WATCHING"
+    }
+  ]
+    setInterval(() => {
+      function presence(){
+        client.user.setPresence({
+          status: "online",
+          activity: array[Math.floor(Math.random() * array.length)]
+        });
+      }
+      presence();
+    }, 10000)//the time is in milli-seconds so in this case, the status will change in 10 seconds interval, you can change it if you want but dont keep it less than 5 seconds cause that might rate-limit.
+  }
+  else{//change your status here if you want a single status
+     client.Ready = true, 
   client.user.setPresence({
     status: "online",  // You can show online, idle, and dnd
     activity: {
@@ -7,8 +37,10 @@ module.exports = async (client) => {
         type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
     }
 });
+    
+  
+  }
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
 client.RegisterSlashCommands();
 };
-
