f7bf6dd90304c8cf7a5e23faec4a221e5f1645f1
OfficialKakarot <79494585+Kakarot-0690@users.noreply.github.com>
2021-10-05 09:15:26 +0530

    Fixed Perms Which Cause Bugs

--- botconfig.js
+++ botconfig.js
@@ -15,7 +15,7 @@ module.exports = {
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
-  Permissions: 2205280576, //Bot Inviting Permissions
+  Permissions: 2205281600, //Bot Inviting Permissions
   Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
   //Lavalink
