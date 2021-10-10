1cc717b1d6c8cfab7d51628b5a885a755b102afc
Sudhan <sudhanplayz@gmail.com>
2021-10-10 09:19:17 +0000

    e

--- api/Routes/webhook.js
+++ api/Routes/webhook.js
@@ -1,11 +1,23 @@
 const api = require("express").Router();
-const Auth = require("../Middlewares/AuthWebhook")
+const Auth = require("../Middlewares/AuthWebhook");
 
-api.use(Auth)
+api.use(Auth);
 
 api.post("/restart", async (req, res) => {
-  console.log("wanted to restart")
-  res.status(400).send("Test")
+  res.status(400).send("Success");
+
+  if (process.env.process_restarting) {
+    delete process.env.process_restarting;
+    // Give old process one second to shut down before continuing ...
+    setTimeout(main, 1000);
+    return;
+  }
+
+  // Restart process ...
+  spawn("git", "pull", "&&", process.argv[0], process.argv.slice(1), {
+    env: { process_restarting: 1 },
+    stdio: "ignore",
+  }).unref();
 });
 
 module.exports = api;
