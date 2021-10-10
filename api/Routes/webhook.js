d5d9e5738778a5814d62ff7efb80314ecb5da149
Sudhan <sudhanplayz@gmail.com>
2021-10-10 10:12:55 +0000

    rm: webhook restart

--- api/Routes/webhook.js
+++ api/Routes/webhook.js
@@ -5,20 +5,9 @@ const { spawn } = require("child-process")
 api.use(Auth);
 
 api.post("/restart", async (req, res) => {
-  res.status(400).send("Success");
-
-  if (process.env.process_restarting) {
-    delete process.env.process_restarting;
-    // Give old process one second to shut down before continuing ...
-    setTimeout(main, 1000);
-    return;
-  }
-
-  // Restart process ...
-  spawn("git", "pull", "&&", process.argv[0], process.argv.slice(1), {
-    env: { process_restarting: 1 },
-    stdio: "ignore",
-  }).unref();
+  res.status(300).send("Success");
+  //todo: Restart the process somehow
+  console.log("Restart webhook has pinged")
 });
 
 module.exports = api;
