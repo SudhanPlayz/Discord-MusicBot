90e9411148dadcba788cdfb3e88cb3c465578ec2
Tech Genius <74496810+TechGenius7777@users.noreply.github.com>
2021-10-12 13:14:03 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ module.exports = async (client) => {
   client.user.setPresence({
     status: client.botconfig.Presence.status, // You can show online, idle, and dnd
       activity: {
-        name: client.botconfig.Presence.name, // The message shown
-        type: client.botconfig.Presence.type, // PLAYING, WATCHING, LISTENING, STREAMING
+        name: client.botconfig.Presence.name,
+        type: client.botconfig.Presence.type,
       },
 });
     client.Manager.init(client.user.id);
