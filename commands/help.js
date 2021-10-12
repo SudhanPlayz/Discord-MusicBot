9526a4286c671f18538e6227b17bd2ea9e9fafcb
Tech Genius <74496810+TechGenius7777@users.noreply.github.com>
2021-10-12 09:05:31 +0530

    Added Dashboard Link In Description In help.js

--- commands/help.js
+++ commands/help.js
@@ -39,7 +39,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
