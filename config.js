2104d7c690c9dc31a2f489b5d9b139bfb21b985c
darren <darrennathanaelofficial@gmail.com>
2021-10-13 06:01:15 -0400

    idk

--- config.js
+++ config.js
@@ -8,5 +8,7 @@ module.exports = {
         retryAmount: "",
         retryDelay: "",
         secure: false
-    }]//Lavalink servers
+    }],//Lavalink servers
+    IconURL: "https://github.com/SudhanPlayz/Discord-MusicBot/raw/master/assets/logo.gif",//This icon will be in every embed's author field
+    "24/7": false,//If you wanted to keep your bot in voice channel withour leaving
 }
