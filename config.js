3ddb68803546095c53f3e5a41d13b5bb530b57fe
Sudhan <sudhanplayz@gmail.com>
2021-10-13 09:05:23 +0000

    sus stuff
    Co-authored-by: Darren <darrennathanaelofficial@gmail.com>

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: process.env.Token || "",//Bot's Token
+    nodes: [{
+        identifier: "Main",
+        host: "",
+        port: 80,
+        password: "",
+        retryAmount: "",
+        retryDelay: "",
+        secure: false
+    }]//Lavalink servers
+}//config.js better ;-; @DarrenOfficial 
---
+++ index.js
@@ -0,0 +1,7 @@
+const DiscordMusicBot = require("./lib/DiscordMusicBot"); 
+const client = new DiscordMusicBot();
+
+client.build() // Make sure your botconfig.js is filled out :p
+
+module.exports = client;//We will just have interaction commands here
+//Bruh ok good luck, Don't remove my dashboard tho lol
