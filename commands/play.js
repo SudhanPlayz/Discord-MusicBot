2b2bdfdb90711523d3450ebe70c6e586305343ef
Kakarot-0690 <79494585+Kakarot-0690@users.noreply.github.com>
2021-10-14 20:32:39 +0530

    Removing Unnessary Comments

--- commands/play.js
+++ commands/play.js
@@ -222,7 +222,7 @@ module.exports = {
       const guild = client.guilds.cache.get(interaction.guild_id);
       const member = guild.members.cache.get(interaction.member.user.id);
       const voiceChannel = member.voice.channel;
-      let awaitchannel = client.channels.cache.get(interaction.channel_id); /// thanks Reyansh for this idea ;-;
+      let awaitchannel = client.channels.cache.get(interaction.channel_id);
       if (!member.voice.channel)
         return client.sendTime(
           interaction,
