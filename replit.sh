de7d4abccfc4570c75e67360427fd6b13ef472b4
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2021-10-14 21:45:51 +0800

    Update replit.sh
    Your command install v16 but you said installing v14

--- replit.sh
+++ replit.sh
@@ -1,4 +1,4 @@
-echo Downloading NodeJS 14
+echo Downloading NodeJS 16
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 npm i
 echo running bot.
