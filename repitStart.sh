be3cd1964e506adc6102eab26f52d4d11a5b0ab5
DarrenOfficial <github@darrennathanael.com>
2021-10-16 17:59:16 -0400

    npm rebuild replit

--- repitStart.sh
+++ repitStart.sh
@@ -1,5 +1,5 @@
 echo Installing nodejs 16
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
-echo Running the bot
+npm rebuild
 npm i
 npm run start
