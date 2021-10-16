4012da8bcb693d0d98fd48f26ff3a7120afa7806
Sudhan <sudhanplayz@gmail.com>
2021-10-16 13:14:44 +0000

    Some new stuff

--- config.js
+++ config.js
@@ -1,14 +1,31 @@
 module.exports = {
     token: process.env.Token || "",//Bot's Token
+    clientId: "",//ID of the bot
+    clientSecret: "",//Client Secret of the bot
+    port: 3000,//Port of the API and Dashboard
+    scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+    serverDeafen: true, //If you want bot to stay deafened
+    defaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+    supportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+    permissions: 2205281600, //Bot Inviting Permissions
     nodes: [{
         identifier: "Main",
         host: "",
         port: 80,
         password: "",
-        retryAmount: "",
-        retryDelay: "",
-        secure: false
+        //retryAmount: 5, - Optional 
+        //retryDelay: 1000, - Optional 
+        //secure: false - Optional | Default: false
     }],//Lavalink servers
-    IconURL: "https://github.com/SudhanPlayz/Discord-MusicBot/raw/master/assets/logo.gif",//This icon will be in every embed's author field
-    "24/7": false,//If you wanted to keep your bot in voice channel withour leaving
+    embedColor: "BLUE",//Color of the embeds
+    presence: { //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+        status: "idle",// You can have online, idle, and dnd
+        activities: [
+            {
+                name: "Music",//Status Text
+                type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+            }
+        ]
+    },
+    iconURL: "https://github.com/SudhanPlayz/Discord-MusicBot/raw/master/assets/logo.gif",//This icon will be in every embed's author field
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DiscordMusicBot = require("./lib/DiscordMusicBot"); 
 const client = new DiscordMusicBot();
 
-client.build() // Make sure your config.js is filled out :p
+// Make sure your config.js is filled out
 
 module.exports = client;
