35bebda4c153a04cb8446917fc2a8de2833c8d13
Sudhan <sudhanplayz@gmail.com>
2021-10-19 06:47:26 +0000

    feat: Guild commands deployment

---
+++ deploy/guild.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const getConfig = require('../util/getConfig');
+const LoadCommands = require("../util/loadCommands");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+(async () => {
+    const config = await getConfig()
+    const rest = new REST({ version: '9' }).setToken(config.token);
+    const commands = await LoadCommands().then(cmds => cmds.map(cmd => cmd.command))
+    
+    rl.question('Enter the guild id you wanted to deploy commands ', (guild) => {
+        console.log("Deploying commands to guild...")
+        await rest.put(Routes.applicationGuildCommands(config.clientId, guild), { body: commands });
+        console.log("Successfully deployed commands!")
+        rl.close();
+    });
+})();
