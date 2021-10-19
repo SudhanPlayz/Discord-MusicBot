2bd03cce8b87c6fab490189eb52a3823b5db2187
Sudhan <sudhanplayz@gmail.com>
2021-10-19 10:41:31 +0000

    *reaslising everythiing*

---
+++ commands/slash/hi.js
@@ -0,0 +1,26 @@
+const {
+  SlashCommandBuilder,
+  SlashCommandUserOption,
+} = require("@discordjs/builders");
+
+module.exports = {
+  command: new SlashCommandBuilder()
+    .setName(Properties.name)
+    .setDescription(Properties.description)
+    .addUserOption(
+      new SlashCommandUserOption()
+        .setRequired(true)
+        .setName("user")
+        .setDescription("User who you wanted to tell hello")
+    ),
+
+  /**
+   * This function will handle slash command interaction
+   * @param {import("../../lib/DiscordMusicBot")} client
+   * @param {import("discord.js").CommandInteraction} interaction
+   * @param {import("discord.js").CommandInteractionOptionResolver} options
+   */
+  run: (client, interaction, options) => {
+    interaction.reply(`<@${options.getUser("user", true).id}>, Hello!`);
+  },
+};
