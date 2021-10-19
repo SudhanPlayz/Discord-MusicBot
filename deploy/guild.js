dddf49cedd651e53edb12de0bcc232d77403ea84
Sudhan <sudhanplayz@gmail.com>
2021-10-19 11:12:06 +0000

    another intent

--- deploy/guild.js
+++ deploy/guild.js
@@ -16,12 +16,15 @@ const rl = readline.createInterface({
     return [].concat(cmds.slash).concat(cmds.context);
   });
 
-  rl.question("Enter the guild id you wanted to deploy commands: ", async (guild) => {
-    console.log("Deploying commands to guild...");
-    await rest.put(Routes.applicationGuildCommands(config.clientId, guild), {
-      body: commands,
-    });
-    console.log("Successfully deployed commands!");
-    rl.close();
-  });
+  rl.question(
+    "Enter the guild id you wanted to deploy commands: ",
+    async (guild) => {
+      console.log("Deploying commands to guild...");
+      await rest.put(Routes.applicationGuildCommands(config.clientId, guild), {
+        body: commands,
+      });
+      console.log("Successfully deployed commands!");
+      rl.close();
+    }
+  );
 })();
