a5a8ead96c668c2525d801adec355e81a0c0cb57
Sudhan <sudhanplayz@gmail.com>
2021-10-19 06:48:25 +0000

    Imagine a commit

--- deploy/global.js
+++ deploy/global.js
@@ -1,14 +1,18 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const getConfig = require('../util/getConfig');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const getConfig = require("../util/getConfig");
 const LoadCommands = require("../util/loadCommands");
 
 (async () => {
-    const config = await getConfig()
-    const rest = new REST({ version: '9' }).setToken(config.token);
-    const commands = await LoadCommands().then(cmds => cmds.map(cmd => cmd.command))
-    
-    console.log("Deploying commands to global...")
-    await rest.put(Routes.applicationCommands(config.clientId), { body: commands });
-    console.log("Successfully deployed commands!")
+  const config = await getConfig();
+  const rest = new REST({ version: "9" }).setToken(config.token);
+  const commands = await LoadCommands().then((cmds) =>
+    cmds.map((cmd) => cmd.command)
+  );
+
+  console.log("Deploying commands to global...");
+  await rest.put(Routes.applicationCommands(config.clientId), {
+    body: commands,
+  });
+  console.log("Successfully deployed commands!");
 })();
--- util/loadCommands.js
+++ util/loadCommands.js
@@ -1,9 +1,9 @@
-const path = require("path")
-const fs = require("fs")
+const path = require("path");
+const fs = require("fs");
 
 const LoadCommands = () => {
   return new Promise((resolve) => {
-    let commands = []
+    let commands = [];
     let CommandsDir = path.join(__dirname, "..", "commands");
     let i = 0,
       f = 0,
@@ -27,12 +27,12 @@ const LoadCommands = () => {
               file.split(".")[0] +
               ", File doesn't have either name/desciption or an command to run"
           );
-        if(cmd.Command.SlashCommand)commands.push(cmd.Command.SlashCommand)
-        if(cmd.Command.ContextMenu)commands.push(cmd.Command.ContextMenu)
+        if (cmd.Command.SlashCommand) commands.push(cmd.Command.SlashCommand);
+        if (cmd.Command.ContextMenu) commands.push(cmd.Command.ContextMenu);
         if (r) resolve(commands);
       });
     });
   });
-}
+};
 
 module.exports = LoadCommands;
