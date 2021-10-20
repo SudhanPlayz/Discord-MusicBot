725dff123e4a2a2cd16ea95f3391f43ec5fd8f0b
Yasoga Nanayakkarawasam <63655428+Psycho649@users.noreply.github.com>
2021-10-20 22:05:10 +0530

    Create config.js

--- config.js
+++ config.js
@@ -4,33 +4,32 @@ module.exports = {
   clientSecret: process.env.clientSecret || "", //Client Secret of the bot
   port: 3000, //Port of the API and Dashboard
   scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-  serverDeafen: true, //If you want bot to stay deafened
-  defaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+  serverDeafen: process.env.sdeafen || "", //If you want bot to stay deafened
+  defaultVolume: process.env.dvolume || "", //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   supportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
   permissions: 2205281600, //Bot Inviting Permissions
   nodes: [
     {
       identifier: "Main",
-      host: "",
-      port: 80,
-      password: "",
+      host: process.env.llhost || "", //lavalink host
+      port: process.env.llport || "", //lavalink port
+      password: process.env.llpass || "", //lavalink password
       //retryAmount: 5, - Optional
       //retryDelay: 1000, - Optional
       //secure: false - Optional | Default: false
     },
   ], //Lavalink servers
-  embedColor: "BLUE", //Color of the embeds
+  embedColor: process.env.ecolor || "", //Color of the embeds
   presence: {
     //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-    status: "idle", // You can have online, idle, and dnd
+    status: process.env.status || "", // You can have online, idle, and dnd
     activities: [
       {
-        name: "Music", //Status Text
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
-        url: "https://twitch.tv/jeffbezos", // Link streaming url
+        name: process.env.statustext || "", //Status Text
+        type: process.env.statustype || "", // PLAYING, WATCHING, LISTENING, STREAMING
+        url: process.env.surl || "", // Link streaming url
       },
     ],
   },
-  iconURL:
-    "https://github.com/SudhanPlayz/Discord-MusicBot/raw/master/assets/logo.gif", //This icon will be in every embed's author field
+  iconURL: process.env.icon || "", //This icon will be in every embed's author field
 };
