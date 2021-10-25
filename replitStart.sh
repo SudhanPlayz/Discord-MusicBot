8fa700078c762cb52e26179670b1192438851358
Mathiscool <82745607+testbot-github@users.noreply.github.com>
2021-10-25 12:12:29 +0530

    updated replitStart.sh
    🤷‍♂️ updated cuz i'm bored and also sudhan will not accept this pull request lmao

--- replitStart.sh
+++ replitStart.sh
@@ -1,5 +1,6 @@
 echo Installing nodejs 16
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+echo Installed nodejs 16
 npm rebuild
 npm i
 npm run start
