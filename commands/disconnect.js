1c816e30a04a0269f3f6cd2a9b275fea3b4c4d20
Swarup <swarupdaz@gmail.com>
2021-10-31 06:39:14 +0530

    added fuckoff alias for disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -8,7 +8,7 @@ module.exports = {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
     member: [],
   },
-  aliases: ["leave", "exit", "quit", "dc", "stop"],
+  aliases: ["leave", "exit", "quit", "dc", "stop", "fuckoff"],
   /**
    *
    * @param {import("../structures/DiscordMusicBot")} client
