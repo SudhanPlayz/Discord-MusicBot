e984399c0ae9e4ac9bd2c2cd57942503eaa8c97f
SirEpiclyAmazing <74148830+SirEpiclyAmazing@users.noreply.github.com>
2021-10-31 12:10:26 +0000

    Update hi.js

--- commands/slash/hi.js
+++ commands/slash/hi.js
@@ -4,7 +4,7 @@ const command = new SlashCommand()
   .setName("hi")
   .setDescription("this is a nice description")
   .setRun((client, interaction, options) => {
-    interaction.reply(`<@${options.getUser("user", true).id}>, Hello!`);
+    interaction.reply(`<@${options.getUser("user", true).id}>, Hello! How are you today?`);
   });
 
 module.exports = command;
