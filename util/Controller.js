bdeb2f51841f6579abbd9087f65abe2f6954f30c
Kakarot-0690 <79494585+Kakarot-0690@users.noreply.github.com>
2021-11-02 11:50:33 +0530

    Added SetTimeout For Button Controls
    lol

--- util/Controller.js
+++ util/Controller.js
@@ -8,25 +8,39 @@ module.exports = async (client, interaction) => {
   let property = interaction.customId.split(":")[2];
   let player = client.manager.get(guild.id);
 
-  if (!player)
-    return interaction.reply({
+  if (!player){
+     interaction.reply({
       embeds: [client.Embed("There is no player to control in this server.")],
     });
-
+    setTimeout(() => {
+      interaction.deleteReply()
+    }, 5000);
+    return;
+  }
   if (property === "LowVolume") {
     player.setVolume(player.volume - 10);
-    return interaction.reply({
+     interaction.reply({
       embeds: [
         client.Embed("Successfully set server volume to " + player.volume),
       ],
     });
+    setTimeout(() => {
+      interaction.deleteReply()
+    }, 5000);
+    return;
+    
   }
 
   if (property === "Replay") {
     if (!player.queue.previous)
-      return interaction.reply({
+        interaction.reply({
         embeds: [client.ErrorEmbed("There is no previous played song")],
-      });
+      }).then(() => {
+        setTimeout(() => {
+      interaction.deleteReply()
+    }, 5000);
+     });
+    
     player.queue.unshift(player.queue.previous);
     player.queue.unshift(player.queue.current);
     player.stop();
@@ -36,9 +50,13 @@ module.exports = async (client, interaction) => {
   if (property === "PlayAndPause") {
     if (player.paused) player.pause(false);
     else player.pause(true);
-    return interaction.reply({
+     interaction.reply({
       embeds: [client.Embed(player.paused ? "Paused" : "Resumed")],
-    });
+    })
+    setTimeout(() => {
+      interaction.deleteReply()
+    }, 5000);
+    return;
   }
 
   if (property === "Next") {
@@ -48,11 +66,15 @@ module.exports = async (client, interaction) => {
 
   if (property === "HighVolume") {
     player.setVolume(player.volume + 10);
-    return interaction.reply({
+    interaction.reply({
       embeds: [
         client.Embed("Successfully set server volume to " + player.volume),
       ],
-    });
+    })
+    setTimeout(() => {
+      interaction.deleteReply()
+    }, 5000);
+    return;
   }
 
   return interaction.reply({
