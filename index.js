25dcafa5b50b473e5ad23438733bdcbc22a7f76c
DarrenOfficial <github@darrennathanael.com>
2021-11-13 10:29:15 -0500

    🚀 cookies

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const client = new DiscordMusicBot();
 console.log("Make sure to fill in the config.js before starting the bot.");
 
 module.exports = client;
+
+// !Bugs to fix: Previous button no worky
