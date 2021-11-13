eeed2efa6f56e32684f818c5fb5e4bf23f5db11e
DarrenOfficial <github@darrennathanael.com>
2021-11-13 10:22:18 -0500

    🚀 status stats?

--- commands/slash/stats.js
+++ commands/slash/stats.js
@@ -1,11 +1,15 @@
 const SlashCommand = require("../../lib/SlashCommand");
-//const erelastats = require("erela.js");
+const erelastats = require("erela.js");
 const { MessageEmbed } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
 const os = require('os');
-// get node stats from erelajs
+const prettyMs = require("pretty-ms");
 
+
+const duration = moment
+.duration(process.uptime())
+.format(" D[d], H[h], m[m]");
 
 
 const uptime = moment.duration(os.uptime() * 1000).format(" D [days], H [hrs], m [mins], s [secs]");
@@ -21,7 +25,7 @@ const command = new SlashCommand()
         .setTitle(`Stats from` + ` \`${client.user.username}\``)
         .setFields([
             { name: ":ping_pong: Ping", value: `┕\`${client.ws.ping}ms\``, inline: true },
-            { name: ":clock1: Uptime", value: `┕\`${uptime}\``, inline: true },
+            { name: ":clock2: Uptime", value: `┕\`${duration}\``, inline: true },
             { name: ":file_cabinet: Memory", value: `┕\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\``, inline: true },
             { name: ":homes: Servers", value: `┕\`${client.guilds.cache.size}\``, inline: true },
             { name: ":green_book: Node", value: `┕\`${process.version}\``, inline: true },
@@ -29,13 +33,16 @@ const command = new SlashCommand()
         ])
         // display node stats
         // TODO: add erela stats and fix total players
-        .setDescription(`\`\`\`css
-Currently Playing : ${client.manager.nodes.values().next().value.stats.playingPlayers}
-Total Players : ${client.manager.nodes.values().next().value.stats.totalPlayers}
-\`\`\``)
-        .setColor(client.config.color)
-
-    // send the embed using interaction reply
+        // display cpu usage in persentage
+        // display uptime of lavalink
+        .setDescription(`\`\`\`fix
+Total Players :: ${client.manager.nodes.values().next().value.stats.playingPlayers} out of ${client.manager.nodes.values().next().value.stats.players} connected clients
+CPU Usage :: ${client.manager.nodes.values().next().value.stats.cpu.lavalinkLoad.toFixed(2) * 100}%
+\`\`\`
+`)
+    // Credits to https://dorndev.net/
+    // Send the embed using interaction reply
+    // TODO: @SudhanPlaz Fix this ok? just make color works thx!
     return interaction.reply({ embeds: [embed] });
     })
 
