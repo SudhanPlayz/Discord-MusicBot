6ae67bffcc709020177d589c7e6aeddab3cbde2c
SudhanPlayz <55418697+sudhanplayz@users.noreply.github.com>
2021-12-04 13:23:33 +0000

    style(cc01e0d): Prettified the codes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const DiscordMusicBot = require("./lib/DiscordMusicBot");
 const client = new DiscordMusicBot();
 
-      console.log("Make sure to fill in the config.js before starting the bot.")
+console.log("Make sure to fill in the config.js before starting the bot.");
 
 module.exports = client;
 
