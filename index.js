fdb0e26703e3758648144d91d68f54db6934e2e0
Darren <darrennathanaelofficial@gmail.com>
2021-12-16 16:00:43 +0000

    Fixed timeline
    run.

--- index.js
+++ index.js
@@ -4,5 +4,3 @@ const client = new DiscordMusicBot();
 console.log("Make sure to fill in the config.js before starting the bot.");
 
 module.exports = client;
-
-// !Bugs to fix: Previous button no worky
