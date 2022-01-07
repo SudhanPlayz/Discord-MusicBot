ef513aa65207c29fafcbc12325c82f2d0df49830
hypwreck1 <90232327+hypwreck1@users.noreply.github.com>
2022-01-08 07:10:29 +0800

    Changes in discord.js 13.4.0+

--- commands/slash/play.js
+++ commands/slash/play.js
@@ -62,7 +62,8 @@ const command = new SlashCommand()
         player.play();
       let embed = client
         .Embed()
-        .setAuthor("Added to queue", client.config.iconURL)
+        .setAuthor({ name: "Added to queue", iconURL: client.config.iconURL })
+        //.setAuthor("Added to queue", client.config.iconURL) Deprecated soon
         .setDescription(
           `[${res.tracks[0].title}](${res.tracks[0].uri})` || "No Title"
         )
