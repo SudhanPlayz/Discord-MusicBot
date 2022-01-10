1c8744815c0d269e9ac49688ef0b5bd8f68bde5f
dottodottoo <90232327+dottodottoo@users.noreply.github.com>
2022-01-10 18:24:04 +0800

    Delete replitStart.sh

--- replitStart.sh
+++
@@ -1,5 +0,0 @@
-echo Installing nodejs 16
-npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
-npm rebuild
-npm i
-npm run start
