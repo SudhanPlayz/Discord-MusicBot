7d442dbdc5c498f974b8d304cd735151cbe01f83
unknown <sudhanplayz@gmail.com>
2022-01-26 11:43:03 +0530

    fix: all errors

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import DiscordMusicBot from "./lib/DiscordMusicBot.js";
 const client = new DiscordMusicBot();
 
-console.log("Make sure to fill in the config.js before starting the bot.");
-
 export default client;
