58ff5f2d8a0d266e07da8cec81526855bd509d1e
DarrenOfficial <github@darrennathanael.com>
2022-01-26 21:27:09 -0500

    Revert PR

--- commands/slash/247.js
+++ commands/slash/247.js
@@ -5,7 +5,29 @@ const command = new SlashCommand()
   .setName("247")
   .setDescription("toggles 24/7")
   .setRun(async (client, interaction, options) => {
-    const player = interaction.client.manager.players.get(interaction.guild.id);
+    let player = client.manager.players.get(interaction.guild.id);
+    if (!interaction.member.voice.channel) {
+      const JoinEmbed = new MessageEmbed()
+        .setColor(client.config.embedColor)
+        .setDescription(
+          "❌ | **You need to join voice channel first before you can use this command.**"
+        );
+      return interaction.reply({ embeds: [JoinEmbed], ephemeral: true });
+    }
+
+    if (
+      interaction.guild.me.voice.channel &&
+      !interaction.guild.me.voice.channel.equals(
+        interaction.member.voice.channel
+      )
+    ) {
+      const SameEmbed = new MessageEmbed()
+        .setColor(client.config.embedColor)
+        .setDescription(
+          "❌ | **You must be in the same voice channel as me.**"
+        );
+      return interaction.reply({ embeds: [SameEmbed], ephemeral: true });
+    }
     if (!player) {
       return interaction.reply({
         embeds: [client.ErrorEmbed("**There's nothing to play 24/7!**")],
