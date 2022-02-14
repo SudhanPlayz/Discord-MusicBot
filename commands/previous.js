68e75fc8e2b97bde2abf39a54911fbd623e9c566
Gooi Hean Zhen <hz.gooi@outlook.com>
2022-02-14 09:57:56 +0800

    Update previous.js

--- commands/previous.js
+++ commands/previous.js
@@ -8,7 +8,7 @@ module.exports = {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
     member: [],
   },
-  aliases: ["cl", "cls"],
+  aliases: ["prev", "back"],
   /**
    *
    * @param {import("../structures/DiscordMusicBot")} client
