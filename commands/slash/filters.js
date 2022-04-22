e60f8304d2ace6e40b0929577493809df3d55952
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 14:37:29 +0530

    Update filters.js

--- commands/slash/filters.js
+++ commands/slash/filters.js
@@ -37,9 +37,7 @@ const command = new SlashCommand()
     if (!interaction.member.voice.channel) {
       const joinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | You must be in a voice channel to use this command!"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
     }
 
@@ -51,9 +49,7 @@ const command = new SlashCommand()
     ) {
       const sameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | You must be in the same voice channel as the bot to use this command!"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
     }
 
