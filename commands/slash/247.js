359eb29abaa8557e5f8f9fd7f380dea3f7ca1ab1
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 14:31:58 +0530

    adding all the scopes

--- commands/slash/247.js
+++ commands/slash/247.js
@@ -9,9 +9,7 @@ const command = new SlashCommand()
     if (!interaction.member.voice.channel) {
       const joinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You need to join voice channel first before you can use this command.**"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
     }
 
@@ -23,9 +21,7 @@ const command = new SlashCommand()
     ) {
       const sameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You must be in the same voice channel as me.**"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
     }
     if (!player) {
