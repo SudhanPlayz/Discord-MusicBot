a81a5e5c3998a5bb0134b40711aec63207f4f3dd
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 15:08:21 +0530

    only a few more!

--- commands/slash/replay.js
+++ commands/slash/replay.js
@@ -9,16 +9,14 @@ const command = new SlashCommand()
     if (!player) {
       const QueueEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription("There's nothing playing in the queue");
+        .setDescription(client.config.QueueEmbed4);
       return interaction.reply({ embeds: [QueueEmbed], ephemeral: true });
     }
 
     if (!interaction.member.voice.channel) {
       const JoinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "You have to join voice channel first before you can use this command"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [JoinEmbed], ephemeral: true });
     }
 
@@ -30,9 +28,7 @@ const command = new SlashCommand()
     ) {
       const SameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "You must be in the same voice channel as me first before you can use this command"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [SameEmbed], ephemeral: true });
     }
 
