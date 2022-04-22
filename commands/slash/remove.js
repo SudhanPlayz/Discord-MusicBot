a8da311d60cea9a31dd0c52556056443ba5b7449
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 15:06:49 +0530

    half way to full project here complete

--- commands/slash/remove.js
+++ commands/slash/remove.js
@@ -18,16 +18,14 @@ const command = new SlashCommand()
     if (!player) {
       const queueEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription("There's nothing playing in the queue");
+        .setDescription(client.config.QueueEmbed4);
       return interaction.reply({ embeds: [queueEmbed], ephemeral: true });
     }
 
     if (!interaction.member.voice.channel) {
       const joinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "You have to join voice channel first before you can use this command"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
     }
 
@@ -39,9 +37,7 @@ const command = new SlashCommand()
     ) {
       const sameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "You must be in the same voice channel as me before you can use this command"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
     }
 
