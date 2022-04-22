7fcad8cf60713e53db3b45bc5eb1353dac26a81e
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 14:35:25 +0530

    Update disconnect.js

--- commands/slash/disconnect.js
+++ commands/slash/disconnect.js
@@ -14,9 +14,7 @@ const command = new SlashCommand()
     if (!interaction.member.voice.channel) {
       const joinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You must be in a voice channel to use this command!**"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
     }
 
@@ -28,9 +26,7 @@ const command = new SlashCommand()
     ) {
       const sameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You must be in the same voice channel as me to use this command!**"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
     }
 
