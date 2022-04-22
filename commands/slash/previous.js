8575ab19402017486a52539834224f4f6512bc7a
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-04-22 15:00:54 +0530

    Update previous.js

--- commands/slash/previous.js
+++ commands/slash/previous.js
@@ -16,9 +16,7 @@ const command = new SlashCommand()
     if (!interaction.member.voice.channel) {
       const joinEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You must be in a voice channel to use this command.**"
-        );
+        .setDescription(client.config.JoinEmbed);
       return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
     }
 
@@ -30,9 +28,7 @@ const command = new SlashCommand()
     ) {
       const sameEmbed = new MessageEmbed()
         .setColor(client.config.embedColor)
-        .setDescription(
-          "❌ | **You must be in the same voice channel as me to use this command!**"
-        );
+        .setDescription(client.config.SameEmbed);
       return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
     }
 
@@ -41,7 +37,7 @@ const command = new SlashCommand()
         embeds: [
           new MessageEmbed()
             .setColor(client.config.embedColor)
-            .setDescription("❌ | **There is no previous song in the queue.**"),
+            .setDescription(client.config.PreviousEmbed),
         ],
       });
 
