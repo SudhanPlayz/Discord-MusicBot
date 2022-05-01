7f049b1ad847c60ffb8e4a1d8e77bded132364f2
Safa A <sawadall@asu.edu>
2022-04-30 17:33:33 -0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "OTcwMDkyNzU2OTAwOTA0OTYw.Ym27SA.66CfVKyAYOgfQGGyfBMxcODgSPY", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "970092756900904960", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "VHGC7r0q3pqazLVzZTbHd3qAvPnvNedq", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
@@ -22,7 +22,7 @@ module.exports = {
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
+    name: "Green Badge Tears ", // The message shown
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
@@ -30,15 +30,15 @@ module.exports = {
   // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "lavalink.darrenofficial.com", //- The host name or IP of the lavalink server.
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "hisudhan", //- The password of the lavalink server.
+    host: "lavalink.oops.wtf", //- The host name or IP of the lavalink server.
+    port: 2000, // The port that lavalink is listening to. This must be a number!
+    pass: "www.freelavalink.ga", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "5d042f5d97064c99aeb5265172b97792", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "16607e9012004f429d3533eae9607267", //Spotify Client Secret
   },
 };
