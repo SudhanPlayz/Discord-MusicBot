29dbc3227b394200c051d5ca0f14cff7d96ab416
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-05-11 18:44:25 +0530

    Grammatical Errors

--- commands/slash/seek.js
+++ commands/slash/seek.js
@@ -77,7 +77,7 @@ const command = new SlashCommand()
       let thing = new MessageEmbed()
         .setColor(client.config.embedColor)
         .setDescription(
-          `Cannot seek current playing track. This may happened because seek duration has exceeded track duration`
+          `Cannot seek the current playing track. This may have happened because the seek duration has exceeded track duration`
         );
       return interaction.editReply({ embeds: [thing] });
     }
