f67a4f994f1d564388cd9e3cce06821c0a7c696f
Darren <git@darrennathanael.com>
2022-06-01 06:08:14 -0400

    Move everything to root.
    Signed-off-by: Darren <git@darrennathanael.com>

--- index.js
+++
@@ -1,6 +0,0 @@
-const DiscordMusicBot = require("./lib/DiscordMusicBot");
-const client = new DiscordMusicBot();
-
-console.log("Make sure to fill in the config.js before starting the bot.");
-
-module.exports = client;
--- replit.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-	];
-}
