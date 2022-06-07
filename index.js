cfcadfa25753b1de19f3c2e060cac9837fcb07bd
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2022-06-07 13:02:27 +0200

    fix hanging login on replit systems

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const DiscordMusicBot = require("./lib/DiscordMusicBot");
+const { exec } = require("child_process");
 const client = new DiscordMusicBot();
+
+if (process.env.REPL_ID) {
+	console.log("Replit system detected, initiating special `unhandledRejection` event listener | index.js:19")
+	process.on('unhandledRejection', (reason, promise) => {
+		promise.catch((err) => {
+			if (err.status === 429) { 
+				console.log("something went wrong while logging in, resetting..."); 
+				exec("kill 1"); 
+			}
+		});
+	}); 
+}
 
 console.log("Make sure to fill in the config.js before starting the bot.");
 
