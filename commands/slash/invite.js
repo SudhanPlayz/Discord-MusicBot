36bad650b1314663081b79ebfefaab43aaa806a8
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-06-10 15:44:16 +0530

    Update invite.js (#874)
    * new look for invite.js
    
    * idk why the gap is still there, in the code there are no lines left

--- commands/slash/invite.js
+++ commands/slash/invite.js
@@ -1,16 +1,19 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 const SlashCommand = require("../../lib/SlashCommand");
 
 const command = new SlashCommand()
-  .setName("invite")
-  .setDescription("Invite me to your server")
-  .setRun(async (client, interaction, options) => {
-    const embed = new MessageEmbed()
-      .setColor(client.config.embedColor)
-      .setTitle(`Invite me to your server`)
-      .setDescription(
-        `You can invite me to your server by clicking [here](https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=bot%20applications.commands)`
-      );
-    return interaction.reply({ embeds: [embed] });
-  });
-module.exports = command;
+.setName("invite")
+.setDescription("Invite me to your server")
+.setRun(async (client, interaction, options) => {
+    return interaction.reply({ 
+        embeds: [new MessageEmbed()
+            .setColor(client.config.embedColor)
+            .setTitle(`Invite me to your server!`)],
+        components: [new MessageActionRow()
+            .addComponents(new MessageButton()
+            .setLabel("Invite me")
+            .setStyle("LINK")
+            .setURL(`https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=${client.config.scopes.toString().replace(',', '%20')}`))
+        ]
+    });
+});
