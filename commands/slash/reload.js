820d0495af1d34bfb2509560062f25daf53189de
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-06-19 11:17:17 +0530

    Changes requested by Darren

--- commands/slash/reload.js
+++ commands/slash/reload.js
@@ -77,7 +77,7 @@ const command = new SlashCommand()
         embeds: [
           new MessageEmbed()
             .setColor(client.config.embedColor)
-            .setTitle("**You are not authorized to use this command!**"),
+            .setDescription("**You are not authorized to use this command!**"),
         ],
         ephemeral: true,
       });
