65928dcff9e3d03bff8bf1bedf135d80c96c853e
JotaroKujo0525 <cloudandreibacor@gmail.com>
2022-06-20 17:31:59 +0800

    Env

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const DiscordMusicBot = require("./structures/DiscordMusicBot");
 const { exec } = require("child_process");
 const client = new DiscordMusicBot();
 
 if (process.env.REPL_ID) {
-	console.log("Replit system detected, initiating special `unhandledRejection` event listener")
-	process.on('unhandledRejection', (reason, promise) => {
-		promise.catch((err) => {
-			if (err.status === 429) { exec("kill 1") }
-		});
-	}); 
+  console.log(
+    "Replit system detected, initiating special `unhandledRejection` event listener"
+  );
+  process.on("unhandledRejection", (reason, promise) => {
+    promise.catch((err) => {
+      if (err.status === 429) {
+        exec("kill 1");
+      }
+    });
+  });
 }
 
 client.build();
