d03d49467547cb8897c278db2f433a64d9800ba6
cobianic <vitrage@protonmail.com>
2022-06-27 02:18:09 +0300

    two spaces to one tab

--- commands/slash/previous.js
+++ commands/slash/previous.js
@@ -5,63 +5,63 @@ const command = new SlashCommand()
 .setName("previous")
 .setDescription("Go back to the previous song.")
 .setRun(async (client, interaction) => {
-  let channel = await client.getChannel(client, interaction);
-  if (!channel) {
-    return;
-  }
+	let channel = await client.getChannel(client, interaction);
+	if (!channel) {
+		return;
+	}
 
-  let player;
-  if (client.manager) {
-    player = client.manager.players.get(interaction.guild.id);
-  } else {
-    return interaction.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor("RED")
-          .setDescription("Lavalink node is not connected"),
-      ],
-    });
-  }
+	let player;
+	if (client.manager) {
+		player = client.manager.players.get(interaction.guild.id);
+	} else {
+		return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor("RED")
+					.setDescription("Lavalink node is not connected"),
+			],
+		});
+	}
 
-  if (!player) {
-    return interaction.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor("RED")
-          .setDescription("There are no previous songs for this session."),
-      ],
-      ephemeral: true,
-    });
-  }
+	if (!player) {
+		return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor("RED")
+					.setDescription("There are no previous songs for this session."),
+			],
+			ephemeral: true,
+		});
+	}
 
-  const previousSong = player.queue.previous;
-  const currentSong = player.queue.current;
-  const nextSong = player.queue[0]
+	const previousSong = player.queue.previous;
+	const currentSong = player.queue.current;
+	const nextSong = player.queue[0]
 
-  if (!previousSong
-    || previousSong === currentSong
-    || previousSong === nextSong) {
-    return interaction.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor("RED")
-          .setDescription("There is no previous song in the queue."),
-      ],
-    })}
+	if (!previousSong
+		|| previousSong === currentSong
+		|| previousSong === nextSong) {
+		return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor("RED")
+					.setDescription("There is no previous song in the queue."),
+			],
+		})}
 
-  if (previousSong !== currentSong && previousSong !== nextSong) {
-    player.queue.splice(0, 0, currentSong)
-    player.play(previousSong);
-  }
-  interaction.reply({
-    embeds: [
-      new MessageEmbed()
-        .setColor(client.config.embedColor)
-        .setDescription(
-          `⏮ | Previous song: **${ previousSong.title }**`,
-        ),
-    ],
-  });
+	if (previousSong !== currentSong && previousSong !== nextSong) {
+		player.queue.splice(0, 0, currentSong)
+		player.play(previousSong);
+	}
+	interaction.reply({
+		embeds: [
+			new MessageEmbed()
+				.setColor(client.config.embedColor)
+				.setDescription(
+					`⏮ | Previous song: **${ previousSong.title }**`,
+				),
+		],
+	});
 });
 
 module.exports = command;
--- util/Controller.js
+++ util/Controller.js
@@ -5,143 +5,143 @@ const { MessageEmbed } = require("discord.js");
  * @param {import("discord.js").ButtonInteraction} interaction
  */
 module.exports = async (client, interaction) => {
-  let guild = client.guilds.cache.get(interaction.customId.split(":")[1]);
-  let property = interaction.customId.split(":")[2];
-  let player = client.manager.get(guild.id);
+	let guild = client.guilds.cache.get(interaction.customId.split(":")[1]);
+	let property = interaction.customId.split(":")[2];
+	let player = client.manager.get(guild.id);
 
-  if (!player) {
-    await interaction.reply({
-      embeds: [
-        client.Embed("❌ | **There is no player to control in this server.**"),
-      ],
-    });
-    setTimeout(() => {
-      interaction.deleteReply();
-    }, 5000);
-    return;
-  }
-  if (!interaction.member.voice.channel) {
-    const joinEmbed = new MessageEmbed()
-      .setColor(client.config.embedColor)
-      .setDescription(
-        "❌ | **You must be in a voice channel to use this action!**",
-      );
-    return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
-  }
+	if (!player) {
+		await interaction.reply({
+			embeds: [
+				client.Embed("❌ | **There is no player to control in this server.**"),
+			],
+		});
+		setTimeout(() => {
+			interaction.deleteReply();
+		}, 5000);
+		return;
+	}
+	if (!interaction.member.voice.channel) {
+		const joinEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setDescription(
+				"❌ | **You must be in a voice channel to use this action!**",
+			);
+		return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
+	}
 
-  if (
-    interaction.guild.me.voice.channel &&
-    !interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)
-  ) {
-    const sameEmbed = new MessageEmbed()
-      .setColor(client.config.embedColor)
-      .setDescription(
-        "❌ | **You must be in the same voice channel as me to use this action!**",
-      );
-    return await interaction.reply({ embeds: [sameEmbed], ephemeral: true });
-  }
+	if (
+		interaction.guild.me.voice.channel &&
+		!interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)
+	) {
+		const sameEmbed = new MessageEmbed()
+			.setColor(client.config.embedColor)
+			.setDescription(
+				"❌ | **You must be in the same voice channel as me to use this action!**",
+			);
+		return await interaction.reply({ embeds: [sameEmbed], ephemeral: true });
+	}
 
-  if (property === "Stop") {
-    player.queue.clear();
-    player.stop();
-    client.warn(`Player: ${ player.options.guild } | Successfully stopped the player`);
-    const msg = await interaction.channel.send({
-      embeds: [
-        client.Embed(
-          "⏹️ | **Successfully stopped the player**",
-        ),
-      ],
-    });
-    setTimeout(() => {
-      msg.delete();
-    }, 5000);
+	if (property === "Stop") {
+		player.queue.clear();
+		player.stop();
+		client.warn(`Player: ${ player.options.guild } | Successfully stopped the player`);
+		const msg = await interaction.channel.send({
+			embeds: [
+				client.Embed(
+					"⏹️ | **Successfully stopped the player**",
+				),
+			],
+		});
+		setTimeout(() => {
+			msg.delete();
+		}, 5000);
 
-    interaction.update({
-      components: [client.createController(player.options.guild, player)],
-    });
-    return;
-  }
+		interaction.update({
+			components: [client.createController(player.options.guild, player)],
+		});
+		return;
+	}
 
-  // if theres no previous song, return an error.
-  if (property === "Replay") {
-    const previousSong = player.queue.previous;
-    const currentSong = player.queue.current;
-    const nextSong = player.queue[0]
+	// if theres no previous song, return an error.
+	if (property === "Replay") {
+		const previousSong = player.queue.previous;
+		const currentSong = player.queue.current;
+		const nextSong = player.queue[0]
 
-    if (!previousSong
-      || previousSong === currentSong
-      || previousSong === nextSong) {
-      const msg = await interaction.channel.send({
-        embeds: [
-          new MessageEmbed()
-          .setColor("RED")
-          .setDescription("There is no previous song in the queue."),
-        ],
-      });
-      setTimeout(() => {
-        msg.delete();
-      }, 5000);
-      return interaction.deferUpdate();
-    }
-    if (previousSong !== currentSong && previousSong !== nextSong) {
-      player.queue.splice(0, 0, currentSong)
-      player.play(previousSong);
-      return interaction.deferUpdate();
-    }
-  }
+		if (!previousSong
+			|| previousSong === currentSong
+			|| previousSong === nextSong) {
+			const msg = await interaction.channel.send({
+				embeds: [
+					new MessageEmbed()
+					.setColor("RED")
+					.setDescription("There is no previous song in the queue."),
+				],
+			});
+			setTimeout(() => {
+				msg.delete();
+			}, 5000);
+			return interaction.deferUpdate();
+		}
+		if (previousSong !== currentSong && previousSong !== nextSong) {
+			player.queue.splice(0, 0, currentSong)
+			player.play(previousSong);
+			return interaction.deferUpdate();
+		}
+	}
 
-  if (property === "PlayAndPause") {
-    if (!player || (!player.playing && player.queue.totalSize === 0)) {
-      const msg = await interaction.channel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription("There is no song playing right now."),
-        ],
-      });
-      setTimeout(() => {
-        msg.delete();
-      }, 5000);
-      return interaction.deferUpdate();
-    } else {
+	if (property === "PlayAndPause") {
+		if (!player || (!player.playing && player.queue.totalSize === 0)) {
+			const msg = await interaction.channel.send({
+				embeds: [
+					new MessageEmbed()
+						.setColor("RED")
+						.setDescription("There is no song playing right now."),
+				],
+			});
+			setTimeout(() => {
+				msg.delete();
+			}, 5000);
+			return interaction.deferUpdate();
+		} else {
 
-      if (player.paused) {
-        player.pause(false);
-      } else {
-        player.pause(true);
-      }
-      client.warn(`Player: ${ player.options.guild } | Successfully ${ player.paused? "paused" : "resumed" } the player`);
+			if (player.paused) {
+				player.pause(false);
+			} else {
+				player.pause(true);
+			}
+			client.warn(`Player: ${ player.options.guild } | Successfully ${ player.paused? "paused" : "resumed" } the player`);
 
-      return interaction.update({
-        components: [client.createController(player.options.guild, player)],
-      });
-    }
-  }
+			return interaction.update({
+				components: [client.createController(player.options.guild, player)],
+			});
+		}
+	}
 
-  if (property === "Next") {
-    player.stop();
-    return interaction.deferUpdate();
-  }
+	if (property === "Next") {
+		player.stop();
+		return interaction.deferUpdate();
+	}
 
-  if (property === "Loop") {
-    if (player.trackRepeat) {
-      player.setTrackRepeat(false);
-      player.setQueueRepeat(true);
-    } else if (player.queueRepeat) {
-      player.setQueueRepeat(false);
-    } else {
-      player.setTrackRepeat(true);
-    }
-    client.warn(`Player: ${ player.options.guild } | Successfully toggled loop the player`);
+	if (property === "Loop") {
+		if (player.trackRepeat) {
+			player.setTrackRepeat(false);
+			player.setQueueRepeat(true);
+		} else if (player.queueRepeat) {
+			player.setQueueRepeat(false);
+		} else {
+			player.setTrackRepeat(true);
+		}
+		client.warn(`Player: ${ player.options.guild } | Successfully toggled loop the player`);
 
-    interaction.update({
-      components: [client.createController(player.options.guild, player)],
-    });
-    return;
-  }
+		interaction.update({
+			components: [client.createController(player.options.guild, player)],
+		});
+		return;
+	}
 
-  return interaction.reply({
-    ephemeral: true,
-    content: "❌ | **Unknown controller option**",
-  });
+	return interaction.reply({
+		ephemeral: true,
+		content: "❌ | **Unknown controller option**",
+	});
 };
