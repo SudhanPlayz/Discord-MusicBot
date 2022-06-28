b798d8c03ee2e4de589d4fc2bf2af06096434977
JotaroKujo0525 <77835007+JotaroKujo0525@users.noreply.github.com>
2022-06-28 08:16:52 +0800

    Final commit yay!

--- kickstartReplit.sh
+++ kickstartReplit.sh
@@ -1,7 +1,6 @@
-# Because node:event error :p
+# For newbies 
 echo Kickstarting replit
-echo Started installing nodejs 16
-npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
-npm rebuild
+echo Please fill your botconfig.js first before running this
 npm i
-npm run deploy
+node index.js 
+
