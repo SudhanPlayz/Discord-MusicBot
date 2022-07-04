5396bedfd4466fe16cc68d7749b449c8951bc8e4
Andrew <vitrage@protonmail.com>
2022-07-04 10:01:18 +0300

    fix previous.js and the same function in Controller.js (#945)
    * Update previous.js
    
    * Update Controller.js
    
    * revert changes to interactions, fixed old bug
    
    Thanks to @Saud-97
    
    * unneeded change
    
    * Apply suggestions from code review
    
    Co-authored-by: Saud-97 <39028181+Saud-97@users.noreply.github.com>
    
    * fix indentation bug in previous
    
    * combing Controller.js
    
    * two spaces to one tab
    
    Co-authored-by: Saud-97 <39028181+Saud-97@users.noreply.github.com>

--- util/Controller.js
+++ util/Controller.js
@@ -8,7 +8,7 @@ module.exports = async (client, interaction) => {
 	let guild = client.guilds.cache.get(interaction.customId.split(":")[1]);
 	let property = interaction.customId.split(":")[2];
 	let player = client.manager.get(guild.id);
-	
+
 	if (!player) {
 		await interaction.reply({
 			embeds: [
@@ -28,7 +28,7 @@ module.exports = async (client, interaction) => {
 			);
 		return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
 	}
-	
+
 	if (
 		interaction.guild.me.voice.channel &&
 		!interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)
@@ -40,7 +40,7 @@ module.exports = async (client, interaction) => {
 			);
 		return await interaction.reply({ embeds: [sameEmbed], ephemeral: true });
 	}
-	
+
 	if (property === "Stop") {
 		player.queue.clear();
 		player.stop();
@@ -55,33 +55,41 @@ module.exports = async (client, interaction) => {
 		setTimeout(() => {
 			msg.delete();
 		}, 5000);
-		
+
 		interaction.update({
 			components: [client.createController(player.options.guild, player)],
 		});
-		
 		return;
 	}
-	
+
 	// if theres no previous song, return an error.
 	if (property === "Replay") {
-		if (!player.queue.previous) {
+		const previousSong = player.queue.previous;
+		const currentSong = player.queue.current;
+		const nextSong = player.queue[0]
+
+		if (!previousSong
+			|| previousSong === currentSong
+			|| previousSong === nextSong) {
 			const msg = await interaction.channel.send({
-				embeds: [client.Embed("❌ | **There is no previous song to replay.**")],
+				embeds: [
+					new MessageEmbed()
+					.setColor("RED")
+					.setDescription("There is no previous song in the queue."),
+				],
 			});
 			setTimeout(() => {
 				msg.delete();
 			}, 5000);
-			return;
+			return interaction.deferUpdate();
 		}
-		const currentSong = player.queue.current;
-		player.play(player.queue.previous);
-		if (currentSong) {
-			player.queue.unshift(currentSong);
+		if (previousSong !== currentSong && previousSong !== nextSong) {
+			player.queue.splice(0, 0, currentSong)
+			player.play(previousSong);
+			return interaction.deferUpdate();
 		}
-		return;
 	}
-	
+
 	if (property === "PlayAndPause") {
 		if (!player || (!player.playing && player.queue.totalSize === 0)) {
 			const msg = await interaction.channel.send({
@@ -94,29 +102,27 @@ module.exports = async (client, interaction) => {
 			setTimeout(() => {
 				msg.delete();
 			}, 5000);
-			
+			return interaction.deferUpdate();
 		} else {
-			
+
 			if (player.paused) {
 				player.pause(false);
 			} else {
 				player.pause(true);
 			}
 			client.warn(`Player: ${ player.options.guild } | Successfully ${ player.paused? "paused" : "resumed" } the player`);
-			
-			interaction.update({
+
+			return interaction.update({
 				components: [client.createController(player.options.guild, player)],
 			});
 		}
-		
-		return;
 	}
-	
+
 	if (property === "Next") {
 		player.stop();
 		return interaction.deferUpdate();
 	}
-	
+
 	if (property === "Loop") {
 		if (player.trackRepeat) {
 			player.setTrackRepeat(false);
@@ -127,13 +133,13 @@ module.exports = async (client, interaction) => {
 			player.setTrackRepeat(true);
 		}
 		client.warn(`Player: ${ player.options.guild } | Successfully toggled loop the player`);
-		
+
 		interaction.update({
 			components: [client.createController(player.options.guild, player)],
 		});
 		return;
 	}
-	
+
 	return interaction.reply({
 		ephemeral: true,
 		content: "❌ | **Unknown controller option**",
