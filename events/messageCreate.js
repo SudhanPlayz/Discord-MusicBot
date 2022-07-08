3d7f6a3ca6f164a101332d226bd50b4cf4102eb7
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-07-08 17:50:51 +0530

    Improve messageCreate.js
    * 2 new buttons for inviting the bot and joining the support server
    
    * Slightly changed mentionEmbed description

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,30 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = async (client, message) => {
-	const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+    const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+
+    const buttons = new MessageActionRow()
+	.addComponents(
+      new MessageButton()
+		.setStyle("LINK")
+		.setLabel('Invite me')
+		.setURL(`https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=bot%20applications.commands`),
+      new MessageButton()
+  		.setStyle("LINK")
+		.setLabel('Support server')
+		.setURL(`${client.config.supportServer}`),
+                      );
 	
 	if (message.content.match(mention)) {
 		const mentionEmbed = new MessageEmbed()
 			.setColor(client.config.embedColor)
 			.setDescription(
-				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please [reinvite](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=${ client.config.scopes.toString()
-					.replace(/,/g, '%20') }) me with the correct permissions.`,
+				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please re-invite me with the correct permissions using the button below.`,
 			);
 		
 		message.channel.send({
 			embeds: [mentionEmbed],
+			components: [buttons],
 		});
 	}
 };
