effb25d3db513a1d75b29ea6d1543c09848d4223
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-07-09 19:11:05 +0530

    Changes requested by Amiyo
    **Added hyperlink back and Changed invite link**
    
    Still contemplating on adding the dashboard

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,13 +2,14 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = async (client, message) => {
     const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+    const invite = `https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=${client.config.scopes.toString().replace(/,/g, '%20')}`;
 
     const buttons = new MessageActionRow()
 	.addComponents(
       new MessageButton()
 		.setStyle("LINK")
 		.setLabel('Invite me')
-		.setURL(`https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=bot%20applications.commands`),
+		.setURL(invite),
       new MessageButton()
   		.setStyle("LINK")
 		.setLabel('Support server')
@@ -19,7 +20,7 @@ module.exports = async (client, message) => {
 		const mentionEmbed = new MessageEmbed()
 			.setColor(client.config.embedColor)
 			.setDescription(
-				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please re-invite me with the correct permissions using the button below.`,
+				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please [re-invite](invite) me with the correct permissions.`,
 			);
 		
 		message.channel.send({
