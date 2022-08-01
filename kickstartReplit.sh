fd8bb54160dfe2a40829b5bba652fc3bef05ebf8
JotaroKujo0525 <77835007+JotaroKujo0525@users.noreply.github.com>
2022-08-01 11:32:43 +0800

    fix issues in replit v5 (#1021)
    * improve kickstartReplit.sh
    
    used nvm
    
    * Update replit.nix
    
    * Update .replit
    
    so you dont need to input bash kickstartReplit.sh every time

--- kickstartReplit.sh
+++ kickstartReplit.sh
@@ -1,5 +1,15 @@
 echo Kickstarting replit
 echo Please make sure to fill config.js before running this script
+if [ -d "$XDG_CONFIG_HOME/nvm" ]; then
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 npm i
-npm run deploy
+node index
+else
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 16
+npm i
 node index.js
+fi
