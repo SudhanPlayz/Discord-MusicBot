bc6e58c5a9f34351e2ff5dbb48277aa8747c5853
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-08-07 16:37:37 +0530

    Improve messageCreate.js (#981)
    * Improve messageCreate.js
    
    * 2 new buttons for inviting the bot and joining the support server
    
    * Slightly changed mentionEmbed description
    
    * Changes requested by Amiyo
    
    **Added hyperlink back and Changed invite link**
    
    Still contemplating on adding the dashboard

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,31 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = async (client, message) => {
-	const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+    const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+    const invite = `https://discord.com/oauth2/authorize?client_id=${client.config.clientId}&permissions=${client.config.permissions}&scope=${client.config.scopes.toString().replace(/,/g, '%20')}`;
+
+    const buttons = new MessageActionRow()
+	.addComponents(
+      new MessageButton()
+		.setStyle("LINK")
+		.setLabel('Invite me')
+		.setURL(invite),
+      new MessageButton()
+  		.setStyle("LINK")
+		.setLabel('Support server')
+		.setURL(`${client.config.supportServer}`),
+                      );
 	
 	if (message.content.match(mention)) {
 		const mentionEmbed = new MessageEmbed()
 			.setColor(client.config.embedColor)
 			.setDescription(
-				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please [reinvite](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=${ client.config.scopes.toString()
-					.replace(/,/g, '%20') }) me with the correct permissions.`,
+				`My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please [re-invite](invite) me with the correct permissions.`,
 			);
 		
 		message.channel.send({
 			embeds: [mentionEmbed],
+			components: [buttons],
 		});
 	}
 };
