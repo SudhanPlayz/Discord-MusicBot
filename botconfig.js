e0c1c49c6f1395d12d18ab24aeb202d31c479eed
Darren <github@darrennathanael.com>
2022-08-10 09:47:47 -0500

    it died lol

--- botconfig.js
+++ botconfig.js
@@ -30,9 +30,9 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "lava-ny-01.thermalhosting.com", //- The host name or IP of the lavalink server.
-    port: 4018, // The port that lavalink is listening to. This must be a number!
-    pass: "thermalhosting.com", //- The password of the lavalink server.
+    host: "", //- The host name or IP of the lavalink server.
+    port: 80, // The port that lavalink is listening to. This must be a number!
+    pass: "", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
