8c864188a5625c8a5dca05632cbc4befa2ed58da
Neko-Life <nekolife123579@gmail.com>
2022-09-08 13:11:07 +0700

    Add base json db

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const { exec } = require("child_process");
+const { get, set } = require("../util/db");
+
 /**
  *
  * @param {import("../lib/DiscordMusicBot")} client
@@ -6,4 +9,14 @@ module.exports = (client) => {
 	client.manager.init(client.user.id);
 	client.user.setPresence(client.config.presence);
 	client.log("Successfully Logged in as " + client.user.tag);
+
+	exec("util/generate-musicbot-id '" + client.user.id + "' '" + client.token + "'", (e, o) => {
+		if (e) console.error(e);
+		if (o.length) {
+			const d = get("global") || {};
+			d.musicbot_id = o.slice(0, -1);
+
+			set("global", d);
+		}
+	});
 };
