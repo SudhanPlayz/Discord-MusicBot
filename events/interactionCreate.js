2d6442b4810b1e539b878e7fcbc52108676583da
Mex <79592017+XstreamSpeed@users.noreply.github.com>
2022-10-30 22:22:32 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,61 +6,6 @@ const Controller = require("../util/Controller");
  * @param {import("discord.js").Interaction}interaction
  */
 module.exports = async(client, interaction) => {
-
-if (interaction.isAutocomplete()) {
-	switch (interaction.commandName) {
-	case 'play':	
-	  /**
-           * @type {import("discord.js").AutocompleteFocusedOption}
-           */
-          const focused = interaction.options.getFocused(true);
-
-          if (focused.name === "query") {
-            if (focused.value === "") return;
-            /**
-             * @type {SearchResult}
-             */
-            const result = await client.manager.search(
-              focused.value,
-              interaction.user
-            );
-
-            if (result.loadType === "TRACK_LOADED" || "SEARCH_RESULT") {
-              /**
-               * @type {Track[]}
-               */
-              const sliced = result.tracks.slice(0, 5).sort();
-
-              if (
-                focused.value.match(
-                  /(?:https:\/\/open\.spotify\.com\/|spotify:)(?:.+)?(track|playlist|artist|episode|show|album)[\/:]([A-Za-z0-9]+)/ ||
-                    /^(?:https?:\/\/|)?(?:www\.)?deezer\.com\/(?:\w{2}\/)?(track|album|playlist)\/(\d+)/ ||
-                    /^((?:https?:)\/\/)?((?:deezer)\.)?((?:page.link))\/([a-zA-Z0-9]+)/ ||
-                    /(?:https:\/\/music\.apple\.com\/)(?:\w{2}\/)?(track|album|playlist)/g ||
-                    /(http(s|):\/\/music\.apple\.com\/..\/.....\/.*\/([0-9]){1,})\?i=([0-9]){1,}/gim ||
-                    /(?:https?:\/\/)?(?:www.|web.|m.)?(facebook|fb).(com|watch)\/(?:video.php\?v=\d+|(\S+)|photo.php\?v=\d+|\?v=\d+)|\S+\/videos\/((\S+)\/(\d+)|(\d+))\/?/g
-                )
-              ) {
-                  await interaction.respond(
-                    sliced.map((track) => ({
-                      name: track.title,
-                      value: focused.value,
-                    }))
-                  );
-                  return;
-              } else {
-                await interaction.respond(
-                  sliced.map((track) => ({
-                    name: track.title,
-                    value: track.uri,
-                  }))
-                );
-              }
-            } else if (result.loadType === "LOAD_FAILED" || "NO_MATCHES")
-              return;
-          }
-      }				
-  }
 	if (interaction.isCommand()) {
 		let command = client.slashCommands.find(
 			(x) => x.name == interaction.commandName,
@@ -94,4 +39,61 @@ if (interaction.isAutocomplete()) {
 			Controller(client, interaction);
 		}
 	}
+
+if (interaction.isAutocomplete()) {
+	switch (interaction.commandName) {
+		case 'play':	
+	  /**
+           * @type {import("discord.js").AutocompleteFocusedOption}
+           */
+          const focused = interaction.options.getFocused(true);
+
+          if (focused.name === "query") {
+            if (focused.value === "") return;
+            /**
+             * @type {SearchResult}
+             */
+            const result = await client.manager.search(
+              focused.value,
+              interaction.user
+            );
+
+            if (result.loadType === "TRACK_LOADED" || "SEARCH_RESULT") {
+              /**
+               * @type {Track[]}
+               */
+              const sliced = result.tracks.slice(0, 5).sort();
+
+              //yall want this or not?
+	     if (
+                focused.value.match(
+                  /(?:https:\/\/open\.spotify\.com\/|spotify:)(?:.+)?(track|playlist|artist|episode|show|album)[\/:]([A-Za-z0-9]+)/ ||
+                    /^(?:https?:\/\/|)?(?:www\.)?deezer\.com\/(?:\w{2}\/)?(track|album|playlist)\/(\d+)/ ||
+                    /^((?:https?:)\/\/)?((?:deezer)\.)?((?:page.link))\/([a-zA-Z0-9]+)/ ||
+                    /(?:https:\/\/music\.apple\.com\/)(?:\w{2}\/)?(track|album|playlist)/g ||
+                    /(http(s|):\/\/music\.apple\.com\/..\/.....\/.*\/([0-9]){1,})\?i=([0-9]){1,}/gim ||
+                    /(?:https?:\/\/)?(?:www.|web.|m.)?(facebook|fb).(com|watch)\/(?:video.php\?v=\d+|(\S+)|photo.php\?v=\d+|\?v=\d+)|\S+\/videos\/((\S+)\/(\d+)|(\d+))\/?/g
+                )
+              ) {
+                  await interaction.respond(
+                    sliced.map((track) => ({
+                      name: track.title,
+                      value: focused.value,
+                    }))
+                  );
+                  return;
+              } else {
+                await interaction.respond(
+                  sliced.map((track) => ({
+                    name: track.title,
+                    value: track.uri,
+                  }))
+                );
+              }
+            } else if (result.loadType === "LOAD_FAILED" || "NO_MATCHES")
+              return;
+          }
+      }				
+  }
+
 };
