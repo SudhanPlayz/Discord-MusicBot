d4fd549f5f5e8654fab99883b72dc4e161128904
Mateus Bertan <contato@mateusbertan.com>
2022-11-19 13:55:47 -0300

    .addField is deprecated
    I changed .addField to .addFields

--- commands/slash/help.js
+++ commands/slash/help.js
@@ -58,16 +58,16 @@ const command = new SlashCommand()
 		);
 		
 		tempArray.forEach((cmd) => {
-			helpEmbed.addField(cmd.name, cmd.description);
+			helpEmbed.addFields({name: cmd.name, value: cmd.description});
 		});
-		helpEmbed.addField(
-			"Credits",
-			`Discord Music Bot Version: v${
+		helpEmbed.addFields({
+			name: "Credits",
+			value: `Discord Music Bot Version: v${
 				require("../../package.json").version
 			}; Build: ${ gitHash }` +
 			"\n" +
 			`[✨ Support Server](${ client.config.supportServer }) | [Issues](${ client.config.Issues }) | [Source](https://github.com/SudhanPlayz/Discord-MusicBot/tree/v5) | [Invite Me](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=bot%20applications.commands)`,
-		);
+		});
 		
 		// Construction of the buttons for the embed
 		const getButtons = (pageNo) => {
@@ -112,17 +112,17 @@ const command = new SlashCommand()
 			tempArray.forEach((cmd) => {
 				//console.log(cmd);
 				helpEmbed
-					.addField(cmd.name, cmd.description)
+					.addFields({ name: cmd.name, value: cmd.description})
 					.setFooter({ text: `Page ${ pageNo + 1 } / ${ maxPages }` });
 			});
-			helpEmbed.addField(
-				"Credits",
-				`Discord Music Bot Version: v${
+			helpEmbed.addFields({
+				name: "Credits",
+				value: `Discord Music Bot Version: v${
 					require("../../package.json").version
 				}; Build: ${ gitHash }` +
 				"\n" +
 				`[✨ Support Server](${ client.config.supportServer }) | [Issues](${ client.config.Issues }) | [Source](https://github.com/SudhanPlayz/Discord-MusicBot/tree/v5) | [Invite Me](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=bot%20applications.commands)`,
-			);
+			});
 			await iter.update({
 				embeds: [helpEmbed],
 				components: [getButtons(pageNo)],
