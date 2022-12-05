be89e65ce7a83e4e53610592e4b4c435058e9a74
Assassin654 <80000135+Assassin654@users.noreply.github.com>
2022-12-05 14:54:37 -0600

    Update guildleave.js
    format

--- commands/guildleave.js
+++ commands/guildleave.js
@@ -15,33 +15,33 @@ const command = new SlashCommand()
   )
 	.setRun(async (client, interaction, options) => {
 		if (interaction.user.id === client.config.adminId) {
-            try{
-                const id = interaction.options.getString('id');
+		    try{
+			const id = interaction.options.getString('id');
 
-                if (id.toLowerCase() === 'list'){
-                    client.guilds.cache.forEach((guild) => {
-                        console.log(`${guild.name} | ${guild.id}`);
-                    });
-                    const guild = client.guilds.cache.map(guild => ` ${guild.name} | ${guild.id}`);
-                    try{
-                    	return interaction.reply({content:`Guilds:\n\`${guild}\``, ephemeral: true});
-                    }catch{
-                        return interaction.reply({content:`check console for list of guilds`, ephemeral: true});
-                    }
-                }
+			if (id.toLowerCase() === 'list'){
+			    client.guilds.cache.forEach((guild) => {
+				console.log(`${guild.name} | ${guild.id}`);
+			    });
+			    const guild = client.guilds.cache.map(guild => ` ${guild.name} | ${guild.id}`);
+			    try{
+				return interaction.reply({content:`Guilds:\n\`${guild}\``, ephemeral: true});
+			    }catch{
+				return interaction.reply({content:`check console for list of guilds`, ephemeral: true});
+			    }
+			}
 
-                const guild = client.guilds.cache.get(id);
+			const guild = client.guilds.cache.get(id);
 
-                if(!guild){
-                    return interaction.reply({content: `\`${id}\` is not a valid guild id`, ephemeral:true});
-                }
+			if(!guild){
+			    return interaction.reply({content: `\`${id}\` is not a valid guild id`, ephemeral:true});
+			}
 
-                await guild.leave().then(c => console.log(`left guild ${id}`)).catch((err) => {console.log(err)});
-                return interaction.reply({content:`left guild \`${id}\``, ephemeral: true});
-            }catch (error){
-                console.log(`there was an error trying to leave guild ${id}`, error);
-            }
-        }else {
+			await guild.leave().then(c => console.log(`left guild ${id}`)).catch((err) => {console.log(err)});
+			return interaction.reply({content:`left guild \`${id}\``, ephemeral: true});
+		    }catch (error){
+			console.log(`there was an error trying to leave guild ${id}`, error);
+		    }
+		}else {
 			return interaction.reply({
 				embeds: [
 					new MessageEmbed()
