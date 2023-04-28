31d5b76e88f2fafdc4b27b14eff6fdb5c2387ffc
DoubleA4 <andyalam03@gmail.com>
2023-04-28 15:05:42 +0700

    fix env issue

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	token: process.env.TOKEN || "", //- Bot's Token
+	clientId: process.env.CLIENTID || "", //- ID of the bot
+	clientSecret: process.env.CLIENTSECRET || "", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
