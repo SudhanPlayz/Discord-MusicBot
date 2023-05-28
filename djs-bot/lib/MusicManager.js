d7da27cd8ab68c69e3cec2c3684023130fe96301
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2023-05-28 11:14:18 +0200

    feat: added connection to lavalink wrapper

--- djs-bot/lib/MusicManager.js
+++ djs-bot/lib/MusicManager.js
@@ -1,28 +1,21 @@
-const getConfig = require("../util/getConfig");
 const Bot = require("./Bot");
 
-// Possibly?
-/* const { Engine } = require(`./clients/${await getConfig().musicEngine}`) */
-
-
-/**
- * Music Manager
- * =============
- * The class is used as an interface with all the music clients in the bot's client folder
- * acts as an API for the music clients and allows for easy switching between clients
- * @returns {MusicManager}
- */
 class MusicManager {
 
 	/**
-	 * @param {Bot} client
-	 */
+	* Music Manager
+	* =============
+	* The class is used as an interface with all the music clients in the bot's client folder
+	* acts as an API for the music clients and allows for easy switching between clients
+	* @param {Bot} client
+	* @returns {MusicManager}
+	*/
 	constructor(client) {
 		/** @type {
 			import('./clients/Erela.js') | 
 			import('./clients/Shoukaku.js')
 		} */
-		this.Engine = require(`./clients/${client.config.musicEngine}`);
+		this.Engine = require(`./clients/${client.config.musicEngine}`)(client);
 	}
 }
 
