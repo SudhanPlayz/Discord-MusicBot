c4b850a6b3fb921bddd043f8c7f08fd9790eb0c4
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2023-05-30 19:14:05 +0200

    feat: added Shoukaku Player methods

--- djs-bot/lib/clients/Shoukaku.js
+++ djs-bot/lib/clients/Shoukaku.js
@@ -1,4 +1,4 @@
-const { Shoukaku, Connectors } = require('shoukaku');
+const { Shoukaku, Connectors, Player } = require('shoukaku');
 const Bot = require('../Bot');
 
 function parseNode(node) {
@@ -21,11 +21,34 @@ export class ShoukakuExtended extends Shoukaku {
 		super(connector, nodes);
 	}
 
-	/** @todo */
-	createPlayer() {}
+	/**
+	 * Create a player for a guild, binds a voice channel to it
+	 * @param {import("discord.js").Guild} guild
+	 * @param {import("discord.js").VoiceBasedChannel} voiceChannel
+	 * 
+	 * @todo Verify if this is the correct way to create a player
+	 */
+	createPlayer(guild, voiceChannel) {
+		const players = this.players;
+		const player = players.get(guild.id) ||
+			new Player(this.leastUsedNode, {
+				guildID: guild.id,
+				shardId: guild.shardId,
+				channelId: voiceChannel.id
+			});
 
-	/** @todo */
-	get leastUsedNode() {}
+		if (!players.has(guild.id))
+			players.set(guild.id, player);
+
+		return player;
+	}
+
+	/**
+	 * Returns the least used node from the array configured in the config file
+	 */
+	get leastUsedNode() {
+		return this.getNode();
+	}
 }
 
 /**
@@ -35,8 +58,8 @@ export class ShoukakuExtended extends Shoukaku {
  */
 module.exports = (client) => {
 	return new ShoukakuExtended(new Connectors.DiscordJS(client), client.config.nodes.map(parseNode))
-	
-	/* Event listeners */
-	.on('ready', (name) => client.log(`Node ${name} | Lavalink node is connected.`))
-	.on('error', (name, error) => client.error(`Node ${name} | Lavalink node has encountered an error: ${error}`));
+
+		/* Event listeners */
+		.on('ready', (name) => client.log(`Node ${name} | Lavalink node is connected.`))
+		.on('error', (name, error) => client.error(`Node ${name} | Lavalink node has encountered an error: ${error}`));
 };
