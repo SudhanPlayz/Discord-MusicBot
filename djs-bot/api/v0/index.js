710f9eacdec2df9b874887da9b60d9e3df878c1d
BioCla <brian.ferri19@gmail.com>
2023-06-19 20:33:48 +0000

    chore: docs

--- djs-bot/api/v0/index.js
+++ djs-bot/api/v0/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 
+// https://expressjs.com/en/starter/installing.html
 const app = express();
 app.get('/', (req, res) => {
 	res.status(200).json({
@@ -9,11 +10,11 @@ app.get('/', (req, res) => {
 	});
 });
 
-// This is the API router
 // It will load all the routes from the `./routes` folder
 // and will mount them on the `/api/v0` path
 // So, for example, if you have a route in `./routes/test.js`
 // it will be mounted on `/api/v0/test`
+// read https://expressjs.com/en/guide/routing.html for more info
 const router = express.Router()
 const routes = fs.readdirSync(__dirname + '/routes').filter(file => file.endsWith('.js'))
 for (const file of routes) {
