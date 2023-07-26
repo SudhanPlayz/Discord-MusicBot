b3d6e69ac3b10226d35b42e674a67772e22d17fe
Neko-Life <nekolife123579@gmail.com>
2023-07-26 20:12:46 +0700

    correct docker run one off container

--- docker/lavalink/standalone.sh
+++ docker/lavalink/standalone.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run -p 2333:2333 -v "./$(dirname "$0")/application.yml:/opt/Lavalink/application.yml" --name lavalink-standalone fredboat/lavalink
-docker container rm lavalink-standalone 2&>/dev/null
+docker run --rm -p 2333:2333 -v "./$(dirname "$0")/application.yml:/opt/Lavalink/application.yml" --name lavalink-standalone fredboat/lavalink
