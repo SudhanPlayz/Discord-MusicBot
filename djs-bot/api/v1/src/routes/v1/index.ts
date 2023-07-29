381942e2bf740fb3eab754c77170565bdf5bc702
Neko-Life <nekolife123579@gmail.com>
2023-07-29 18:19:22 +0700

    check user auth data from db

--- djs-bot/api/v1/src/routes/v1/index.ts
+++ djs-bot/api/v1/src/routes/v1/index.ts
@@ -6,6 +6,7 @@ import type {
 import { readdirSync } from 'fs';
 import { API_ROUTES_PREFIX } from '../../lib/constants';
 import APIError from '../../lib/APIError';
+import * as db from '../../lib/db';
 
 const routes: RegisterRouteHandler = async (app, opts, done) => {
   const routes = readdirSync(__dirname + '/routesHandler').filter((file) =>
@@ -49,12 +50,19 @@ const routes: RegisterRouteHandler = async (app, opts, done) => {
     const rPath = request.routerPath.substring(API_ROUTES_PREFIX.length);
 
     if (routesRequiresAuth.some((path) => rPath === path)) {
-      if (!request.headers.user_id?.length)
+      const throwError = () => {
         throw new APIError(
           'Unauthorized',
           APIError.STATUS_CODES.UNAUTHORIZED,
           APIError.ERROR_CODES.UNAUTHORIZED,
         );
+      };
+
+      if (!request.headers.user_id?.length) throwError();
+
+      const auth = await db.getUserAuth(request.headers.user_id as string);
+
+      if (!auth?.access_token?.length) throwError();
     }
   });
 
