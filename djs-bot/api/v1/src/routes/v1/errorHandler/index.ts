2d318f713f6eaa09a50c940c74deb18b22dfbd59
Neko-Life <nekolife123579@gmail.com>
2023-08-20 17:01:27 +0700

    pass axios error to client

--- djs-bot/api/v1/src/routes/v1/errorHandler/index.ts
+++ djs-bot/api/v1/src/routes/v1/errorHandler/index.ts
@@ -1,6 +1,7 @@
 import { getBot } from '../../..';
 import type { RouteErrorHandler } from '../../../interfaces/common';
 import APIError from '../../../lib/APIError';
+import axios from 'axios';
 
 const routesErrorHandler: RouteErrorHandler = (err, request, reply) => {
   const bot = getBot(true);
@@ -22,6 +23,19 @@ const routesErrorHandler: RouteErrorHandler = (err, request, reply) => {
       message: 'Go away you pesky hacker',
     });
 
+    return;
+  }
+
+  if (err instanceof axios.AxiosError && err.response) {
+    const { status, data } = err.response;
+
+    reply.status(status).send({
+      success: false,
+      code: status,
+      data,
+      message: data?.error,
+      description: data?.error_description,
+    });
     return;
   }
 
