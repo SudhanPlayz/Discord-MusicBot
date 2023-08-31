588e1e7136098a875160c0c8d2fa5bda64a1de5d
birb <contact@lewdhutao.me>
2023-08-31 23:14:18 +0800

    Update userinfo.js

--- djs-bot/commands/utility/userinfo.js
+++ djs-bot/commands/utility/userinfo.js
@@ -7,9 +7,10 @@ module.exports = {
 	description: "Get information on a user or yourself",
 	options: [
 		{
-			name: 'user',
+			name: "user",
 			type: 6, // "USER"
-			description: 'User you want to get information about. If omitted, will return your information',
+			description:
+				"User you want to get information about. If omitted, will return your information",
 			required: false,
 		},
 	],
@@ -26,26 +27,58 @@ module.exports = {
 		};
 
 		// Filtering out @everyone role and getting the roles of roles for the member
-		const roles = member.roles.cache.map(roles => { if (roles.name != "@everyone") return `<@&${roles.id}>` }).join(' ');
+		const roles = member.roles.cache
+			.map((roles) => {
+				if (roles.name != "@everyone") return `<@&${roles.id}>`;
+			})
+			.join(" ");
 
 		const embed = new EmbedBuilder()
-			.setColor(client.config.embedColor || member.displayHexColor || 'RANDOM')
+			.setColor(client.config.embedColor || member.displayHexColor || "RANDOM")
 			.setThumbnail(target.displayAvatarURL({ dynamic: true }))
-			.setAuthor({ name: `${target.tag} (${target.id})`, iconURL: target.displayAvatarURL({ dynamic: true })})
-			.addField('**User Information**', [
-				`**❯ Username:** ${target.username}`,
-				`**❯ Discriminator:** ${target.discriminator}`,
-				`**❯ ID:** ${target.id}`,
-				`**❯ Avatar:** [Link to avatar](${target.displayAvatarURL({ dynamic: true })})`,
-				`**❯ Time Created:** ${new Date(target.createdTimestamp).toLocaleString()}`,
-				`**❯ Status:** ${status[target.presence?.status]}`,
-				`**❯ Bot:** ${target.bot}`
-			].join('\n').toString())
-			.addField('**Member Information**', [
-				`**❯ Highest Role:** ${member.roles.highest.id === interaction.guild.id ? "None" : `<@&${member.roles.highest.id}>`}`,
-				`**❯ Server Join Date:** ${new Date(member.joinedTimestamp).toLocaleString()}`,
-				`**❯ Roles [${member.roles.cache.size - 1}]:** ${roles}`
-			].join('\n').toString())
+			.setAuthor({
+				name: `${target.tag} (${target.id})`,
+				iconURL: target.displayAvatarURL({ dynamic: true }),
+			})
+			.addFields([
+				{
+					name: "**User Information**",
+					value: [
+						`**❯ Username:** ${target.username}`,
+						`**❯ Discriminator:** ${target.discriminator}`,
+						`**❯ ID:** ${target.id}`,
+						`**❯ Avatar:** [Link to avatar](${target.displayAvatarURL(
+							{ dynamic: true }
+						)})`,
+						`**❯ Time Created:** ${new Date(
+							target.createdTimestamp
+						).toLocaleString()}`,
+						`**❯ Status:** ${status[target.presence?.status]}`,
+						`**❯ Bot:** ${target.bot}`,
+					]
+						.join("\n")
+						.toString(),
+				},
+				{
+					name: "**Member Information**",
+					value: [
+						`**❯ Highest Role:** ${
+							member.roles.highest.id ===
+							interaction.guild.id
+								? "None"
+								: `<@&${member.roles.highest.id}>`
+						}`,
+						`**❯ Server Join Date:** ${new Date(
+							member.joinedTimestamp
+						).toLocaleString()}`,
+						`**❯ Roles [${
+							member.roles.cache.size - 1
+						}]:** ${roles}`,
+					]
+						.join("\n")
+						.toString(),
+				},
+			])
 			.setTimestamp();
 
 		interaction.reply({ embeds: [embed] });
