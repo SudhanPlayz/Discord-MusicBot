f2bbf2efe434bbc0ca8bcb6989082c3bfe150ebd
birb <contact@lewdhutao.me>
2023-08-31 23:12:46 +0800

    Update ping.js

--- djs-bot/commands/utility/ping.js
+++ djs-bot/commands/utility/ping.js
@@ -4,16 +4,34 @@ module.exports = {
 	name: "ping",
 	category: "utility",
 	usage: "/ping",
-	description: "Is the bot running slow? Check the bot's ping and see if it's lagging or if you are!",
+	description:
+		"Is the bot running slow? Check the bot's ping and see if it's lagging or if you are!",
 	ownerOnly: false,
 	run: async (client, interaction) => {
 		const msg = await interaction.channel.send(`🏓 Pinging...`);
-		await interaction.reply({ embeds: [new EmbedBuilder()
-			.setTitle(':signal_strength: PONG!')
-			.addField("BOT", `\`\`\`yml\n${Math.floor(msg.createdAt - interaction.createdAt)}ms\`\`\``, true)
-			.addField("API", `\`\`\`yml\n${client.ws.ping}ms\`\`\``, true)
-			.setColor(client.config.embedColor)
-			.setTimestamp()] });
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(":signal_strength: PONG!")
+					.addFields([
+						{
+							name: "BOT",
+							value: `\`\`\`yml\n${Math.floor(
+								msg.createdAt -
+									interaction.createdAt
+							)}ms\`\`\``,
+							inline: true,
+						},
+						{
+							name: "API",
+							value: `\`\`\`yml\n${client.ws.ping}ms\`\`\``,
+							inline: true,
+						},
+					])
+					.setColor(client.config.embedColor)
+					.setTimestamp(),
+			],
+		});
 		msg.delete();
 	},
 };
