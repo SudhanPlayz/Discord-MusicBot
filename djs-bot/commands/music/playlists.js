86a619c0538a6bf42cddbea792cb0337aa262aa0
Neko-Life <nekolife123579@gmail.com>
2023-09-11 12:41:24 +0700

    untested refactor, TODO: testing

--- djs-bot/commands/music/playlists.js
+++ djs-bot/commands/music/playlists.js
@@ -1,8 +1,9 @@
-const { EmbedBuilder } = require("discord.js");
+"use strict";
+
 const SlashCommand = require("../../lib/SlashCommand");
 const { joinStageChannelRoutine } = require("../../util/player");
 const { capitalize } = require("../../util/string");
-const { colorEmbed } = require("../../util/embeds");
+const { colorEmbed, redEmbed } = require("../../util/embeds");
 const yt = require("youtube-sr").default;
 
 async function testUrlRegex(string) {
@@ -29,7 +30,6 @@ const reply = async (interaction, desc) =>
 
 async function runView(client, interaction, options) {
 	const playlistName = options.getString("playlist_name");
-
 	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
 
 	const playlist = await client.db.playlist.findFirst({
@@ -67,9 +67,11 @@ async function runCreate(client, interaction, options) {
 	const playlistName = options.getString("playlist_name");
 
 	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
-	else if (playlistName.length < 3)
+
+	if (playlistName.length < 3)
 		return reply(interaction, "The playlist name can't be shorter than 3 characters");
-	else if (playlistName.length > 32)
+
+	if (playlistName.length > 32)
 		return reply(interaction, "The playlist name can't be longer than 32 characters");
 
 	const playlist = await client.db.playlist.create({
@@ -94,44 +96,19 @@ async function runCreate(client, interaction, options) {
 
 async function runDelete(client, interaction, options) {
 	const playlistName = options.getString("playlist_name");
+	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
 
-	if (!playlistName)
-		return interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setColor(client.config.embedColor)
-					.setDescription(),
-			],
-			ephemeral: true,
-		});
 	const playlist = await client.db.playlist.findFirst({
 		where: {
 			name: playlistName,
 			userId: interaction.user.id,
 		},
 	});
-	if (!playlist)
-		return interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setColor(client.config.embedColor)
-					.setDescription(
-						"I couldn't find a playlist with that name"
-					),
-			],
-			ephemeral: true,
-		});
+
+	if (!playlist) return reply(interaction, "I couldn't find a playlist with that name");
+
 	if (playlist.userId !== interaction.user.id)
-		return interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setColor(client.config.embedColor)
-					.setDescription(
-						"You can't delete a playlist that isn't yours"
-					),
-			],
-			ephemeral: true,
-		});
+		return reply(interaction, "You can't delete a playlist that isn't yours");
 
 	await client.db.playlist.delete({
 		where: {
@@ -139,14 +116,204 @@ async function runDelete(client, interaction, options) {
 		},
 	});
 
-	return interaction.reply({
-		embeds: [
-			new EmbedBuilder()
-				.setColor(client.config.embedColor)
-				.setDescription(`Deleted playlist **${playlist.name}**`),
-		],
-		ephemeral: true,
+	return reply(interaction, `Deleted playlist **${playlist.name}**`);
+}
+
+async function runAdd(client, interaction, options) {
+	const playlistName = options.getString("playlist_name");
+	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
+
+	const song = options.getString("song", true);
+	if (!song) return reply(interaction, "You need to provide a song to add");
+
+	const playlist = await client.db.playlist.findFirst({
+		where: {
+			name: playlistName,
+			userId: interaction.user.id,
+		},
 	});
+
+	if (!playlist) return reply(interaction, "I couldn't find a playlist with that name");
+
+	if (playlist.userId !== interaction.user.id)
+		return reply(interaction, "You can't add songs to a playlist that isn't yours");
+
+	const songData = await yt.getVideo(song);
+	if (!songData) return reply(interaction, "I couldn't find a song with that name");
+
+	const songExists = await client.db.song.findFirst({
+		where: {
+			name: songData.title,
+			link: songData.url,
+		},
+	});
+
+	if (!songExists) {
+		const newSong = await client.db.song.create({
+			data: {
+				name: songData.title,
+				link: songData.url,
+				artist: songData.channel.name,
+			},
+		});
+
+		await client.db.playlist.update({
+			where: {
+				id: playlist.id,
+			},
+			data: {
+				songs: {
+					connect: {
+						id: newSong.id,
+					},
+				},
+			},
+		});
+	} else {
+		await client.db.playlist.update({
+			where: {
+				id: playlist.id,
+			},
+			data: {
+				songs: {
+					connect: {
+						id: songExists.id,
+					},
+				},
+			},
+		});
+	}
+
+	return reply(
+		interaction,
+		`Added song **${songData.title}** to playlist **${playlist.name}**`
+	);
+}
+
+async function runRemove(client, interaction, options) {
+	const playlistName = options.getString("playlist_name");
+	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
+
+	const song = options.getString("song", true);
+	if (!song) return reply(interaction, "You need to provide a song to remove");
+
+	const playlist = await client.db.playlist.findFirst({
+		where: {
+			name: playlistName,
+			userId: interaction.user.id,
+		},
+	});
+
+	if (!playlist) return reply(interaction, "I couldn't find a playlist with that name");
+	if (playlist.userId !== interaction.user.id)
+		return reply(
+			interaction,
+			"You can't remove songs from a playlist that isn't yours"
+		);
+
+	const songData = await client.db.song.findFirst({
+		where: {
+			name: song,
+		},
+	});
+	if (!songData) return reply(interaction, "I couldn't find a song with that name");
+
+	await client.db.playlist.update({
+		where: {
+			id: playlist.id,
+		},
+		data: {
+			songs: {
+				disconnect: {
+					id: songData.id,
+				},
+			},
+		},
+	});
+
+	return reply(interaction, `Removed **${songData.name}** from **${playlist.name}**`);
+}
+
+async function runPlay(client, interaction, options) {
+	const playlistName = options.getString("playlist_name");
+	if (!playlistName) return reply(interaction, "You need to provide a name for the playlist");
+
+	const playlist = await client.db.playlist.findFirst({
+		where: {
+			name: playlistName,
+			userId: interaction.user.id,
+		},
+	});
+
+	if (!playlist) return reply(interaction, "I couldn't find a playlist with that name");
+
+	if (playlist.userId !== interaction.user.id)
+		return reply(interaction, "You can't play a playlist that isn't yours");
+
+	const songs = await client.db.song.findMany({
+		where: {
+			playlistId: playlist.id,
+		},
+	});
+
+	if (!songs.length) return reply(interaction, "That playlist is empty");
+
+	if (!client.manager.Engine)
+		return redEmbed({
+			desc: "Lavalink node is not connected",
+		});
+
+	const manager = client.manager.Engine;
+
+	const node = await client.getLavalink(client);
+
+	if (!node) {
+		return interaction.reply({
+			embeds: [
+				redEmbed({
+					desc: `No available nodes to play music on!`,
+				})
+					.setTitle("Node error!")
+					.setFooter({
+						text: "Oops! something went wrong but it's not your fault!",
+					}),
+			],
+		});
+	}
+
+	const channel = await client.getChannel(client, interaction);
+	if (!channel) {
+		return;
+	}
+
+	if (!manager.players.get(interaction.guild.id)) {
+		client.manager.Engine.createPlayer({
+			guildId: interaction.guild.id,
+			voiceChannel: channel.id,
+			textChannel: interaction.channel.id,
+		});
+	}
+
+	const player = manager.players.get(interaction.guild.id);
+
+	if (player.state !== "CONNECTED") {
+		player.connect();
+	}
+
+	for (const song of songs) {
+		const songSearch = await player.search(song.link, interaction.user.id);
+		player.queue.add(songSearch.tracks[0]);
+	}
+
+	if (channel.type == "GUILD_STAGE_VOICE") {
+		joinStageChannelRoutine(interaction.guild.members.me);
+	}
+
+	if (!player.playing) {
+		player.play();
+	}
+
+	return reply(interaction, `Playing **${playlist.name}**`);
 }
 
 module.exports = new SlashCommand()
@@ -181,78 +348,75 @@ module.exports = new SlashCommand()
 			.setAutocomplete(true)
 	)
 	.setAutocompleteOptions(async (input, index, interaction, client) => {
-		if (client.db) {
-			if (index == 1) {
-				const operation = interaction.options.getString("operation");
-				if (
-					operation === "delete" ||
-					operation === "view" ||
-					operation === "add" ||
-					operation === "remove" ||
-					operation === "play"
-				) {
-					const playlists = await client.db.playlist.findMany({
-						where: {
-							user: {
-								id: interaction.user.id,
-							},
-						},
-					});
-					return playlists.map((playlist) => {
-						return {
-							name: capitalize(playlist.name),
-							value: playlist.name,
-						};
-					});
-				} else return [];
-			} else if (index == 2) {
-				const operation = interaction.options.getString("operation");
-				if (operation === "remove") {
-					const playlistName =
-						interaction.options.getString("playlist_name");
-					if (!playlistName) return [];
-					const playlist = await client.db.playlist.findFirst({
-						where: {
-							name: playlistName,
-							userId: interaction.user.id,
-						},
-					});
-					if (!playlist) return [];
-					const songs = await client.db.song.findMany({
-						where: {
-							playlistId: playlist.id,
-						},
-					});
-					return songs.map((song) => {
-						return {
-							name: capitalize(song.name),
-							value: song.name,
-						};
-					});
-				} else if (operation === "add") {
-					if (input.length <= 3) return [];
-					if (await testUrlRegex(input))
-						return [{ name: "URL", value: input }];
+		if (!client.db) return [{ name: "DB Unavailable", value: "DB_Error" }];
 
-					const random = "ytsearch"[
-						Math.floor(Math.random() * "ytsearch".length)
-					];
-					const results = await yt.search(input || random, {
-						safeSearch: false,
-						limit: 25,
-					});
+		if (index == 1) {
+			const playlists = await client.db.playlist.findMany({
+				where: {
+					user: {
+						id: interaction.user.id,
+					},
+				},
+			});
 
-					const choices = [];
-					for (const video of results) {
-						choices.push({
-							name: video.title,
-							value: video.url,
-						});
-					}
-					return choices;
-				} else return [];
+			return playlists.map((playlist) => {
+				return {
+					name: capitalize(playlist.name),
+					value: playlist.name,
+				};
+			});
+		}
+
+		if (index == 2) {
+			const operation = interaction.options.getString("operation");
+			if (operation === "remove") {
+				const playlistName = interaction.options.getString("playlist_name");
+				if (!playlistName) return [];
+				const playlist = await client.db.playlist.findFirst({
+					where: {
+						name: playlistName,
+						userId: interaction.user.id,
+					},
+				});
+				if (!playlist) return [];
+				const songs = await client.db.song.findMany({
+					where: {
+						playlistId: playlist.id,
+					},
+				});
+				return songs.map((song) => {
+					return {
+						name: capitalize(song.name),
+						value: song.name,
+					};
+				});
 			}
-		} else return [{ name: "DB Unavailable", value: "DB_Error" }];
+
+			if (operation === "add") {
+				if (input.length <= 3) return [];
+				if (await testUrlRegex(input))
+					return [{ name: "URL", value: input }];
+
+				const random = "ytsearch"[
+					Math.floor(Math.random() * "ytsearch".length)
+				];
+				const results = await yt.search(input || random, {
+					safeSearch: false,
+					limit: 25,
+				});
+
+				const choices = [];
+				for (const video of results) {
+					choices.push({
+						name: video.title,
+						value: video.url,
+					});
+				}
+				return choices;
+			}
+
+			return [];
+		}
 	})
 	.setCategory("music")
 	.setUsage("/playlists")
@@ -270,331 +434,8 @@ module.exports = new SlashCommand()
 			case "add":
 				return runAdd(client, interaction, options);
 			case "remove":
-				break;
+				return runRemove(client, interaction, options);
 			case "play":
-				break;
-		}
-
-		if (operation === "view") {
-		} else if (operation === "create") {
-		} else if (operation === "delete") {
-		} else if (operation === "add") {
-			const playlistName = options.getString("playlist_name");
-			const playlist = await client.db.playlist.findFirst({
-				where: {
-					name: playlistName,
-					userId: interaction.user.id,
-				},
-			});
-
-			if (!playlist)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"I couldn't find a playlist with that name"
-							),
-					],
-					ephemeral: true,
-				});
-			if (playlist.userId !== interaction.user.id)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"You can't add songs to a playlist that isn't yours"
-							),
-					],
-					ephemeral: true,
-				});
-
-			const song = options.getString("song", true);
-			if (!song)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"You need to provide a song to add"
-							),
-					],
-					ephemeral: true,
-				});
-
-			const songData = await yt.getVideo(song);
-			if (!songData)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"I couldn't find a song with that name"
-							),
-					],
-					ephemeral: true,
-				});
-
-			const songExists = await client.db.song.findFirst({
-				where: {
-					name: songData.title,
-					link: songData.url,
-				},
-			});
-
-			if (!songExists) {
-				const newSong = await client.db.song.create({
-					data: {
-						name: songData.title,
-						link: songData.url,
-						artist: songData.channel.name,
-					},
-				});
-
-				await client.db.playlist.update({
-					where: {
-						id: playlist.id,
-					},
-					data: {
-						songs: {
-							connect: {
-								id: newSong.id,
-							},
-						},
-					},
-				});
-			} else {
-				await client.db.playlist.update({
-					where: {
-						id: playlist.id,
-					},
-					data: {
-						songs: {
-							connect: {
-								id: songExists.id,
-							},
-						},
-					},
-				});
-			}
-
-			return interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setColor(client.config.embedColor)
-						.setDescription(
-							`Added song **${songData.title}** to playlist **${playlist.name}**`
-						),
-				],
-				ephemeral: true,
-			});
-		} else if (operation === "remove") {
-			const playlistName = options.getString("playlist_name");
-			const playlist = await client.db.playlist.findFirst({
-				where: {
-					name: playlistName,
-					userId: interaction.user.id,
-				},
-			});
-
-			if (!playlist)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"I couldn't find a playlist with that name"
-							),
-					],
-					ephemeral: true,
-				});
-			if (playlist.userId !== interaction.user.id)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"You can't remove songs from a playlist that isn't yours"
-							),
-					],
-					ephemeral: true,
-				});
-
-			const song = options.getString("song", true);
-			if (!song)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"You need to provide a song to remove"
-							),
-					],
-					ephemeral: true,
-				});
-			const songData = await client.db.song.findFirst({
-				where: {
-					name: song,
-				},
-			});
-			if (!songData)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"I couldn't find a song with that name"
-							),
-					],
-					ephemeral: true,
-				});
-
-			await client.db.playlist.update({
-				where: {
-					id: playlist.id,
-				},
-				data: {
-					songs: {
-						disconnect: {
-							id: songData.id,
-						},
-					},
-				},
-			});
-
-			return interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setColor(client.config.embedColor)
-						.setDescription(
-							`Removed **${songData.name}** from **${playlist.name}**`
-						),
-				],
-				ephemeral: true,
-			});
-		} else if (operation === "play") {
-			const playlistName = options.getString("playlist_name");
-			const playlist = await client.db.playlist.findFirst({
-				where: {
-					name: playlistName,
-					userId: interaction.user.id,
-				},
-			});
-
-			if (!playlist)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"I couldn't find a playlist with that name"
-							),
-					],
-					ephemeral: true,
-				});
-			if (playlist.userId !== interaction.user.id)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription(
-								"You can't play a playlist that isn't yours"
-							),
-					],
-					ephemeral: true,
-				});
-
-			const songs = await client.db.song.findMany({
-				where: {
-					playlistId: playlist.id,
-				},
-			});
-
-			if (!songs.length)
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor(client.config.embedColor)
-							.setDescription("That playlist is empty"),
-					],
-					ephemeral: true,
-				});
-
-			let player;
-			if (client.manager.Engine) {
-				player = client.manager.Engine;
-			} else {
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor("Red")
-							.setDescription(
-								"Lavalink node is not connected"
-							),
-					],
-				});
-			}
-
-			let node = await client.getLavalink(client);
-			if (!node) {
-				return interaction.reply({
-					embeds: [
-						new EmbedBuilder()
-							.setColor("Red")
-							.setTitle("Node error!")
-							.setDescription(
-								`No available nodes to play music on!`
-							)
-							.setFooter({
-								text: "Oops! something went wrong but it's not your fault!",
-							}),
-					],
-				});
-			}
-
-			let channel = await client.getChannel(client, interaction);
-			if (!channel) {
-				return;
-			}
-
-			if (!player.players.get(interaction.guild.id)) {
-				client.manager.Engine.createPlayer({
-					guildId: interaction.guild.id,
-					voiceChannel: channel.id,
-					textChannel: interaction.channel.id,
-				});
-			}
-
-			if (player.players.get(interaction.guild.id).state !== "CONNECTED") {
-				player.players.get(interaction.guild.id).connect();
-			}
-
-			for (const song of songs) {
-				const songSearch = await player.search(
-					song.link,
-					interaction.user.id
-				);
-				player.players
-					.get(interaction.guild.id)
-					.queue.add(songSearch.tracks[0]);
-			}
-
-			if (channel.type == "GUILD_STAGE_VOICE") {
-				joinStageChannelRoutine(interaction.guild.members.me);
-			}
-
-			if (!player.players.get(interaction.guild.id).playing) {
-				player.players.get(interaction.guild.id).play();
-			}
-
-			return interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setColor(client.config.embedColor)
-						.setDescription(`Playing **${playlist.name}**`),
-				],
-				ephemeral: true,
-			});
+				return runPlay(client, interaction, options);
 		}
 	});
