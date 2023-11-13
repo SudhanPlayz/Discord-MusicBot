d8d7a6a28c2ce7bedea25d4c0ffcd95e1aac1816
elweondelosmomos <150608650+elweondelosmomos@users.noreply.github.com>
2023-11-12 19:09:55 -0500

    Config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
+	token: process.env.token || "MTE3MzI4ODAwNTU0MTMxMDY0Ng.Gb-Fg8.dVhRqSyiIJ4CqwfwI4gcTBPSpp4t1sleXytDL0", //- Bot's Token
 	clientId: process.env.clientId || "", //- ID of the bot
 	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
