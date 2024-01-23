79678770acffcedf32b8fda4ce19cc7e409b6047
Filip Pękala <272985@student.pwr.edu.pl>
2024-01-23 14:01:55 +0100

    Connection to a self-hosted lavalink
    Changed config.js to automatically connect to a lavalink from
    a docker-compose image.

--- config.js
+++ config.js
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			host: "lavalink", //- The host name or IP of the lavalink server.
+			port: 2333, // The port that lavalink is listening to. This must be a number!
+			password: "bonsoirDocker", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
