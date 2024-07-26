9de6f5fc0de8c5ba6c00c8ad5864e7587cbd6af3
Zeni <gustavo.zeni@pier.digital>
2024-07-26 01:18:53 -0300

    undo remove 'UserId' placeholder

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "", //- Replace UserId with the Discord ID of the admin of the bot
+	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
 	token: process.env.token || "", //- Bot's Token
 	clientId: process.env.clientId || "", //- ID of the bot
 	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
