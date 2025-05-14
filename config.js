b316ee2b9255c977ba0a46b6ee2c8db852344a21
Hacki <hackk0110@gmail.com>
2025-05-14 19:05:23 +0200

    Config - trackStartMessage
    Ability to disable bot message on new track start in config.json

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ module.exports = {
 	autoQueue: false, //- When set to true, related songs will automatically be added to the queue
 	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
 	autoLeave: false, //- When set to true, the bot will automatically leave when no one is in the voice channel (can be combined with 24/7 to always be in voice channel until everyone leaves; if 24/7 is on disconnectTime will add a disconnect delay after everyone leaves.)
+	trackStartMessage: true, //- When set to true, the bot will send a message when a track starts playing
 	debug: false, //- Debug mode
 	cookieSecret: "CodingWithSudhan is epic", //- Cookie Secret
 	website: "http://localhost:4200", //- without the / at the end
